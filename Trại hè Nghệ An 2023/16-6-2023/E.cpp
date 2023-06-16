e3703eb7fcd6f74cf9158bab73f3f7ee918b7c7d
ntannn <nhattanff07@gmail.com>
2023-06-17 01:53:16 +0700

    ok

--- Trại hè Nghệ An 2023/16-6-2023/E.cpp
+++ Trại hè Nghệ An 2023/16-6-2023/E.cpp
@@ -1,6 +1,37 @@
 #include<bits/stdc++.h>
 //#pragma GCC optimize("O3,unroll-loops,no-stack-protector")
 //#pragma GCC target("sse4,avx2,fma")
+
+using namespace std;
+
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+ 
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ", "; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define dbg(x...) cerr << "\e[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _print(x); cerr << "\e[39m" << endl;
+#else
+#define dbg(x...)
+#endif
+
 #define fi first
 #define se second
 #define mp make_pair
@@ -8,24 +39,6 @@
 #define eb emplace_back
 #define all(x) x.begin(), x.end()
 
-using namespace std;
-
-template <typename T> inline void read (T &x) {
-    bool b = 0;
-    char c;
-    while (!isdigit (c = getchar()) && c != '-');
-    if (c == '-') {
-        c = getchar();
-        b = 1;
-    }
-    x = c - 48;
-    while (isdigit(c = getchar())) {
-        x = (x << 3) + (x << 1) + (c - 48);
-    }
-    if (b) {
-        x=-x;
-    }
-}
 
 typedef long long ll;
 typedef pair<int,int> ii;
@@ -49,14 +62,8 @@ void solve(){
             v[j].pb(a[i]);
         }
     }
+    dbg(v[10])
     for(int i = 1; i <= N; i++) {
-        // if(!b[i].empty() && v[i].empty()) {
-        //     for(auto j : b[i]){
-        //         q.push(j);
-        //         b[i].pop_back();
-        //     }
-        //     //continue;
-        // }
         if(!b[i].empty()) {
             for(auto j : b[i]) {
                 q.push(j);
@@ -73,21 +80,29 @@ void solve(){
                     q.pop();
                 }
             }
+            
         }        
+        if(fre)
+            cout << i << " " << fre << '\n';
+        if(!q.empty())
+            cout << i << " " << q.front() << '\n';
+        while(fre > 0) {
+            ans.eb(i);
+            //q.pop();
+            if(i == 10)
+                cout<<"fre\n";
+            fre--;
+        }
         if(!q.empty()) {
-            while(fre && !q.empty()) {
-                ans.eb(i);
-                fre--;
-                q.pop();
-            }
-            if(!q.empty())
-                for(auto j : v[i]) {
-                    if(!M[j]){
-                        q.pop();
-                        ans.eb(j);
-                        M[j] = 1;
-                    }
+            //cout << q.front() << '\n';
+            for(auto j : v[i]) {
+                if(!M[j]){
+                    //cout << j << '\n';
+                    q.pop();
+                    ans.eb(i);
+                    M[j] = 1;
                 }
+            }                        
         }
     }
     for(auto i : ans)
