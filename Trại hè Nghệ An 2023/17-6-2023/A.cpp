d496c7f3c0f16af6d95d8e910aee46d72c32dd11
ntannn <nhattanff07@gmail.com>
2023-06-17 10:41:45 +0700

    ok

--- Trại hè Nghệ An 2023/17-6-2023/A.cpp
+++ Trại hè Nghệ An 2023/17-6-2023/A.cpp
@@ -9,7 +9,32 @@
 #define all(x) x.begin(), x.end()
 
 using namespace std;
-
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+ 
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ", "; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define dbg(x...) cerr << "\e[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _print(x); cerr << "\e[39m" << endl;
+#else
+#define dbg(x...)
+#endif
 template <typename T> inline void read (T &x) {
     bool b = 0;
     char c;
@@ -39,22 +64,31 @@ typedef unordered_map<ll, ll> umll;
 const int N = 5e5+10;
 const ll MOD = 1e9+7;
 
-int a[N], p[N], n;
+int a[N], p[N], n, ans = 0;
 map<int, int> M;
 vi s;
 void solve(){
     for(int i = 1; i <= n; i++) {
         if(M[i]) continue;
         int j = i;
-        s.eb(i);
+        s.eb(a[i]);
         while(p[j] != i) {
-            s.eb(p[j]);
             j = p[j];
+            s.eb(j);
         }
         int temp = s.size()-1;
+        cout << "l1"; dbg(s)
         for(j = 0; j <= temp; j++)
-            s.eb(j);
+            s.eb(s[j]);
+        for(j = 0; j < s.size()-1; j++)
+            if(!M[s[j]])
+                M[s[j]] = j;
+            else 
+                ans = max(ans, j - M[s[j]]);
+        cout << "l3"; dbg(s)
+        s.clear();
     }
+    cout << ans-1;
 }
 
 void init(){
