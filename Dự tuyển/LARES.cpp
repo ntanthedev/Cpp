df2c1f785fca7cd8a49d7b8921c2d4eedaa669f8
ntannn <nhattanff07@gmail.com>
2023-07-03 08:24:29 +0700

    ok

---
+++ Dự tuyển/LARES.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+//#pragma GCC optimize("O3,unroll-loops,no-stack-protector")
+//#pragma GCC target("sse4,avx2,fma")
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define all(x) x.begin(), x.end()
+
+using namespace std;
+
+template <typename T> inline void read (T &x) {
+    bool b = 0;
+    char c;
+    while (!isdigit (c = getchar()) && c != '-');
+    if (c == '-') {
+        c = getchar();
+        b = 1;
+    }
+    x = c - 48;
+    while (isdigit(c = getchar())) {
+        x = (x << 3) + (x << 1) + (c - 48);
+    }
+    if (b) {
+        x=-x;
+    }
+}
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+const int N = 1e6+10;
+const ll MOD = 1e9+7;
+
+ll res, m, n, k, ans;
+
+void solve(){
+    res = (m+n)-k;
+    if(n <= res/3)
+        ans = n;
+    else if(m <= res/3)
+        ans = m;
+    else {
+        ans = res/3;
+    }
+    cout << ans ;
+}
+
+void init(){
+    cin >> m >> n >> k;
+}
+#define task "LARES"
+int32_t main(){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    if(fopen(task".inp","r")){
+        freopen(task".inp","r",stdin);
+        freopen(task".out","w",stdout);
+    }
+    int test_case = 1;
+    //cin >> test_case;
+    while(test_case--){
+        init();
+        solve();
+    }
+}
