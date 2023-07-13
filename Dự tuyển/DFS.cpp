8e083fe678b52c654fedd2cb149bd45ba14cecfc
ntannn <nhattanff07@gmail.com>
2023-07-13 09:50:17 +0700

    ok

--- Dự tuyển/DFS.cpp
+++ Dự tuyển/DFS.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define fi first
 #define se second
 #define pb push_back
@@ -9,40 +9,41 @@
 
 using namespace std;
 
-template <typename T> inline void read (T &x) {
+template<typename T>
+inline void read(T& x) {
     bool b = 0;
     char c;
-    while (!isdigit (c = getchar()) && c != '-');
-    if (c == '-') {
+    while(!isdigit(c = getchar()) && c != '-')
+        ;
+    if(c == '-') {
         c = getchar();
         b = 1;
     }
     x = c - 48;
-    while (isdigit(c = getchar())) {
+    while(isdigit(c = getchar())) {
         x = x * 10 + (c - 48);
     }
-    if (b) {
-        x=-x;
+    if(b) {
+        x = -x;
     }
 }
 
 typedef long double ld;
 typedef long long ll;
-typedef pair<int,int> ii;
-typedef pair<ll,ll> pll;
+typedef pair<int, int> ii;
+typedef pair<ll, ll> pll;
 typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef vector<vi> vvi;
 typedef vector<vll> vvl;
 typedef vector<ii> vii;
 
-const int MAXN = 1e5+10;
-const ll MOD = 1e9+7;
+const int MAXN = 1e5 + 10;
+const ll MOD = 1e9 + 7;
 
-bool is_prime(int x)
-{
-    for(int i = 2 ; i * i <= x ; i++)
-        if(x%i == 0)
+bool is_prime(int x) {
+    for(int i = 2; i * i <= x; i++)
+        if(x % i == 0)
             return 0;
     return 1;
 }
@@ -52,37 +53,38 @@ vi f[MAXN], ans;
 unordered_map<int, bool> M;
 bool flag = false;
 //----------------------------
-void dfs(int x){
+void dfs(int x) {
     if(flag)
         return;
     M[x] = true;
-    for(int v: f[x]){
-        if(!M[v]){
+    for(int v: f[x]) {
+        if(!M[v]) {
             ans.pb(v);
-            if(v == t){
+            if(v == t) {
                 flag = true;
                 return;
             }
-            dfs(v);if(flag)
-            return;
+            dfs(v);
+            if(flag)
+                return;
             ans.pop_back();
-        }        
+        }
     }
 }
-void solve(){
-    //for(int v:f[s]) cout<<v<<" "; cout<<'\n';
+void solve() {
+    // for(int v:f[s]) cout<<v<<" "; cout<<'\n';
     for(int i = 1; i <= n; i++)
         sort(all(f[i]));
     ans.pb(s);
     dfs(s);
-    for(int i:ans)
-        cout<<i<<" ";
+    for(int i: ans)
+        cout << i << " ";
 }
 
-void init(){
+void init() {
     cin >> n >> m >> s >> t;
     int u, v;
-    for(int i = 1; i <= m; i++){
+    for(int i = 1; i <= m; i++) {
         cin >> u >> v;
         f[u].pb(v);
     }
@@ -90,13 +92,13 @@ void init(){
 //-----TASK----------
 #define task "DFS"
 //-------------------
-int32_t main(){
+int32_t main() {
     //---------------------------------
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
-    if(fopen(task".inp","r")){
-        freopen(task".inp","r",stdin);
-        freopen(task".out","w",stdout);
+    if(fopen(task ".inp", "r")) {
+        freopen(task ".inp", "r", stdin);
+        freopen(task ".out", "w", stdout);
     }
     //---------------------------------
     init();
