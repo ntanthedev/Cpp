ab98ee0096eab5e5dafe4067b368e503f107d6a7
ntannn <nhattanff07@gmail.com>
2023-08-08 14:53:20 +0700

    ok

--- Dự tuyển/Luyen/pink2.cpp
+++ Dự tuyển/Luyen/pink2.cpp
@@ -7,10 +7,29 @@ using namespace std;
 
 typedef long long ll;
 
-
+ll ans = 0, n, a[N], x, dp[N];
 
 void solve() {
-    
+    memset(dp, 0, sizeof(dp));
+    cin >> n;
+    fo(i, 1, n) 
+        cin >> a[i];
+    cin >> x;
+    fo(i, 1, n)
+        a[i] -= x;
+    bool flag = 0; ans = 0;
+    fo(i, 1, n) {
+        if(!flag || dp[i-1] + a[i] >= 0) {
+            ans++;
+            dp[i] = min(dp[i-1] + a[i], a[i]);
+            flag = 1;
+        }
+        else {
+            flag = 0;
+            dp[i] = 0;
+        }
+    }
+    cout << ans << '\n';
 }
 
 int main()
@@ -21,6 +40,8 @@ int main()
         freopen("pink2.inp","r",stdin);
         freopen("pink2.out","w",stdout);
     }
-
-    solve();
+    int test_case;
+    cin >> test_case;
+    while(test_case--)
+        solve();
 }
--- Dự tuyển/a.cpp
+++ Dự tuyển/a.cpp
@@ -1,59 +1,46 @@
 #include<bits/stdc++.h>
-#pragma GCC optimize("O3,unroll-loops,no-stack-protector")
-#pragma GCC target("sse4,avx2,fma")
 
 #define fo(a, b, c) for(int a = b; a <= c; ++a)
 
 const long long N = 1e6+9;
 using namespace std;
 
-template <typename T> inline void read (T &x) {
-    bool b = 0;
-    char c;
-    while (!isdigit (c = getchar()) && c != '-');
-    if (c == '-') {
-        c = getchar();
-        b = 1;
-    }
-    x = c - 48;
-    while (isdigit(c = getchar())) {
-        x = (x << 3) + (x << 1) + (c - 48);
-    }
-    if (b) {
-        x=-x;
-    }
-}
-
 typedef long long ll;
 
-int n, x;
-int M[N];
+ll ans = 0, n, a[N], x, dp[N];
 
 void solve() {
-    int ans = 0;
-    fo(i, 1, N-1)
-        if(M[i] > 1)
-            ans = max(ans, i);
+    cin >> n;
+    fo(i, 1, n) 
+        cin >> a[i];
+    cin >> x;
+    fo(i, 1, n)
+        a[i] -= x;
+    bool flag = 0;
+    fo(i, 1, n) {
+        if(!flag || dp[i-1] + a[i] >= 0) {
+            ans++;
+            dp[i] = min(dp[i-1] + a[i], a[i]);
+            flag = 1;
+        }
+        else {
+            flag = 0;
+            dp[i] = 0;
+        }
+    }
     cout << ans;
 }
 
-int main() 
+int main()
 {
     cin.tie(NULL); cout.tie(NULL);
     ios_base::sync_with_stdio(false);
-    if(fopen("a.inp","r")){
-        freopen("a.inp","r",stdin);
-        freopen("a.out","w",stdout);
+    if(fopen("pink2.inp","r")){
+        freopen("pink2.inp","r",stdin);
+        freopen("pink2.out","w",stdout);
     }
-    read(n);
-    fo(i, 1, n) {
-        read(x);
-        fo(j, 1, sqrt(x))
-            if(x%j == 0) {
-                M[j]++;
-                if(x/j != j)
-                    M[x/j]++;
-            }
-    }
-    solve();
+    int test_case;
+    cin >> test_case;
+    while(test_case--)
+        solve();
 }
