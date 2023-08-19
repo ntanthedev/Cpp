9911f0bbfd9b0f5bb1a7a77d25e3843b641302cb
ntannn <nhattanff07@gmail.com>
2023-08-19 07:54:43 +0700

    .

--- Dự tuyển/More/aa.cpp
+++ Dự tuyển/More/aa.cpp
@@ -1,51 +1,70 @@
 #include <bits/stdc++.h>
+//---definition----
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define pf pop_front
+#define fi first
+#define se second
+#define all(x) x.begin(), x.end()
+#define range(x, l, r) x.begin() + l, x.begin() + r + 1
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2")
+const int N = 1e6 + 21;
+const int T = 1e3 + 21;
+const ll mod = 1e9 + 7;
 using namespace std;
-
-typedef long long ll;
-const ll N = 2e5 + 99;
-
-int n, dp[N];
-vector<int> a[N], last;
-
-bool cmp(vector<int> A, vector<int> B) {
-    return A.back() < B.back();
+//---variation---
+//---pre-solution(function)---
+//---solution---
+void solve() {
+    long long a, b, S = 0, e, f;
+    cin >> a >> b;
+    f = a;
+    e = b - (b % 4);
+    if(a % 4 != 0)
+        f = a + 4 - (a % 4);
+    S += (e - f) / 4 + 1;
+    f = a;
+    e = b - (b % 11);
+    if(a % 11 != 0)
+        f = a + 11 - (a % 11);
+    S += (e - f) / 11 + 1;
+    f = a;
+    e = b - (b % 7);
+    if(a % 7 != 0)
+        f = a + 7 - (a % 7);
+    S += (e - f) / 7 + 1;
+    f = a;
+    e = b - (b % 28);
+    if(a % 28 != 0)
+        f = a + 28 - (a % 28);
+    S -= (e - f) / 28 + 1;
+    f = a;
+    e = b - (b % 77);
+    if(a % 77 != 0)
+        f = a + 77 - (a % 77);
+    S -= (e - f) / 77 + 1;
+    f = a;
+    e = b - (b % 44);
+    if(a % 44 != 0)
+        f = a + 44 - (a % 44);
+    S -= (e - f) / 44 + 1;
+    f = a;
+    e = b - (b % 308);
+    if(a % 308 != 0)
+        f = a + 308 - (a % 308);
+    S += (e - f) / 308 + 1;
+    cout << S;
 }
-
+//---main---
 int main() {
-    ios_base::sync_with_stdio(NULL);
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    cout.tie(NULL);
-
-    cin >> n;
-    for(int i = 1; i <= n; i++) {
-        int k;
-        cin >> k;
-        for(int j = 1; j <= k; j++) {
-            ll u;
-            cin >> u;
-            if(a[i].empty())
-                a[i].push_back(u);
-            else {
-                if(a[i].back() < u)
-                    a[i].push_back(u);
-            }
-        }
+    ll tcase;
+    cin >> tcase;
+    while(tcase--) {
+        solve();
+        cout << '\n';
     }
-
-    sort(a + 1, a + n + 1, cmp);
-
-    last.push_back(0);
-    for(int i = 1; i <= n; i++) {
-        int sz = a[i].size();
-        dp[i] = max(dp[i - 1], sz);
-        int dd = 0;
-        for(auto u: a[i]) {
-            int j = lower_bound(last.begin(), last.end(), u) - last.begin() - 1;
-            dp[i] = max(dp[i], dp[j] + sz - dd);
-            dd++;
-        }
-        last.push_back(a[i].back());
-    }
-
-    cout << dp[n];
 }
--- VNOI/code/origin/code.cpp
+++ VNOI/code/origin/code.cpp
@@ -1,86 +1,21 @@
 #include<bits/stdc++.h>
-#pragma GCC optimize("O3,unroll-loops,no-stack-protector")
-#pragma GCC target("sse4,avx2,fma")
-#define fi first
-#define se second
-#define mp make_pair
-#define pb push_back
-#define eb emplace_back
-#define all(x) x.begin(), x.end()
-
+#define tag "code"
+#define ll long long 
+const long long N = 1e6+9;
 using namespace std;
-
-
-
-template <typename T> inline void read (T &x) {
-    bool b = 0;
-    char c;
-    while (!isdigit (c = getchar()) && c != '-');
-    if (c == '-') {
-        c = getchar();
-        b = 1;
-    }
-    x = c - 48;
-    while (isdigit(c = getchar())) {
-        x = (x << 3) + (x << 1) + (c - 48);
-    }
-    if (b) {
-        x=-x;
-    }
-}
-
-typedef long long ll;
-typedef pair<int,int> ii;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef vector<ii> vii;
-typedef unordered_map<int, int> umii;
-typedef unordered_map<int, bool> umib;
-typedef unordered_map<ll, ll> umll;
-
-const int N = 1e5+10;
-const ll MOD = 1e9+7;
-int n, a[N], x[N];
-priority_queue<ii, vii, greater<ii>>q;
-int dp[N];
+ll n;
 void solve(){
-    q.push(mp(0, 0));
-    for(int i = 1, j = 0; i <= n; i++){
-        while(!q.empty() && q.top().fi <= x[i]) {
-            j = max(j, q.top().se);
-            q.pop();
-            //cout << i << " "; dbg(dp[i]);
-        }
-        dp[i] = min(dp[i], x[i] - x[j]);
-        q.push(mp(dp[i] + x[i], i));
-    }
-    int ans = INT_MAX;
-    for(int i = 1 ; i <= n; i++) {
-        ans = min(ans, max(dp[i], x[n] - x[i]));
-    }
-    cout << ans;
+    cout<<(n+1)*(n+1)-1;
 }
 
-void init(){
-    read(n);
-    for(int i = 1; i <= n; i++){
-        read(a[i]);
-        x[i] = x[i-1] + a[i];
-        dp[i] = INT_MAX;
-    }
-}
-#define task "code"
-int32_t main(){
-    cin.tie(NULL);
+int main()
+{
+    cin.tie(NULL); cout.tie(NULL);
     ios_base::sync_with_stdio(false);
-    if(fopen(task".inp","r")){
-        freopen(task".inp","r",stdin);
-        freopen(task".out","w",stdout);
+    if(fopen(tag".inp","r")){
+        freopen(tag".inp","r",stdin);
+        freopen(tag".out","w",stdout);
     }
-    int test_case = 1;
-    //cin >> test_case;
-    while(test_case--){
-        init();
-        solve();
-    }
+    cin >> n;
+    solve();
 }
