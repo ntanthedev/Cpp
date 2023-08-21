522ffae06ba65dc07fff548dec5313d3a8648133
ntannn <nhattanff07@gmail.com>
2023-08-21 15:05:54 +0700

    ok

---
+++ Dự tuyển/Luyen/IPARD.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+//#pragma GCC optimize("O3,unroll-loops,no-stack-protector")
+//#pragma GCC target("sse4,avx2,fma")
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define all(x) x.begin(), x.end()
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+
+const int N = 1e6+10;
+const ll MOD = 123456789;
+
+ll n, dp[N];
+
+void solve() {
+    dp[0] = 1;
+    for(int i = 1; i <= n; i++) {
+        for(int j = n; j >= i; j--) {
+            dp[j] += dp[j-i] + dp[i];
+        }
+    }
+    cout << dp[n];
+}
+
+void init() {
+    cin >> n;
+}
+
+#define task "IPARD"
+int32_t main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    if(fopen(task".inp","r")) {
+        freopen(task".inp","r",stdin);
+        freopen(task".out","w",stdout);
+    }
+    int test_case = 1;
+    //cin >> test_case;
+    while(test_case--) {
+        init();
+        solve();
+    }
+}
--- Dự tuyển/a.cpp
+++ Dự tuyển/a.cpp
@@ -1,90 +1,27 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+
+#define fo(a, b, c) for(int a = b; a <= c; ++a)
+
+const long long N = 1e6+9;
 using namespace std;
 
-const int N = 1e6+10;
 typedef long long ll;
 
-template <typename T> inline void read (T &x) {
-    bool b = 0;
-    char c;
-    while (!isdigit (c = getchar()) && c != '-');
-    if (c == '-') {
-        c = getchar();
-        b = 1;
-    }
-    x = c - 48;
-    while (isdigit(c = getchar())) {
-        x = (x << 3) + (x << 1) + (c - 48);
-    }
-    if (b) {
-        x=-x;
-    }
+
+
+void solve() {
+    
 }
 
-ll node[N], lazy[N], res[N];
-
-void day(ll id, ll l, ll r) {
-    if(res[id] == 1) {
-        node[id * 2] = node[id * 2 + 1] = 0;
-        lazy[id * 2] = lazy[id * 2 + 1] = 0;
-        res[id * 2] = res[id * 2 + 1] = 1;
-    }
-    res[id] = 0;
-    int mid = (l + r)/2;
-    lazy[id * 2] ^= lazy[id];
-    lazy[id * 2 + 1] ^= lazy[id];
-    if((mid - l + 1) % 2 == 1) {
-        node[id * 2] ^= lazy[id];
-    }
-    if((r - mid) % 2 == 1) {
-        node[id * 2 + 1] ^= lazy[id];
-    }
-    lazy[id] = 0;
-}
-
-void update(ll id, ll l, ll r, ll d, ll c, ll f) {
-    if((l > r) || (l > c) || (r < d)) {
-        return;
-    }
-    if((d <= l) && (r <= c)) {
-        if((r - l + 1) % 2 == 1)
-            node[id] ^= f;
-        lazy[id] ^= f;
-        return;
-    }
-    int mid = (l + r)/2;
-    day(id, l, r);
-    update(id * 2, l, mid, d, c, f);
-    update(id * 2 + 1, mid + 1, r, d, c, f);
-    node[id] = node[id * 2] ^ node[id * 2 + 1];
-}
-
-ll get_xor(ll id, ll l, ll r, ll d, ll c) {
-    if(l > c || r < d) {
-        return 0;
-    }
-    if((d <= l) && (r <= c))
-        return node[id];
-    int mid = (l + r)/2;
-    day(id, l, r);
-    return (get_xor(id * 2, l, mid, d, c) ^ get_xor(id * 2 + 1, mid + 1, r, d, c));
-}
-
-int32_t main() {
+#define task "a"
+int main()
+{
+    cin.tie(NULL); cout.tie(NULL);
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    ll n, Q, c, l, r, f;
-    read(n), read(Q);
-    while(Q--) {
-        int c;
-        read(c);
-        if(c == 1) {
-            read(l), read(r), read(f);
-            update(1, 1, n, l, r, f);
-        } else {
-            read(l), read(r);
-            cout << get_xor(1, 1, n, l, r) << '\n';
-            node[1] = 0, res[1] = 1, lazy[1] = 0;
-        }
+    if(fopen(task".inp","r")){
+        freopen(task".inp","r",stdin);
+        freopen(task".out","w",stdout);
     }
+
+    solve();
 }
