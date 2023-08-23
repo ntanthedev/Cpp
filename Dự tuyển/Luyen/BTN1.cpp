afed4e24f29cb491cd35f0fb26b9bf6b0075395a
ntannn <nhattanff07@gmail.com>
2023-08-23 07:33:21 +0700

    .

--- Dự tuyển/Luyen/BTN1.cpp
+++ Dự tuyển/Luyen/BTN1.cpp
@@ -1,6 +1,6 @@
-#include<bits/stdc++.h>
-//#pragma GCC optimize("O3,unroll-loops,no-stack-protector")
-//#pragma GCC target("sse4,avx2,fma")
+#include <bits/stdc++.h>
+// #pragma GCC optimize("O3,unroll-loops,no-stack-protector")
+// #pragma GCC target("sse4,avx2,fma")
 #define fi first
 #define se second
 #define mp make_pair
@@ -11,37 +11,63 @@
 using namespace std;
 
 typedef long long ll;
-typedef pair<int,int> ii;
-typedef pair<ll,ll> pll;
+typedef pair<int, int> ii;
+typedef pair<ll, ll> pll;
 typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef vector<ii> vii;
 typedef vector<vi> vvi;
 
-const int N = 1e6+10;
-const ll MOD = 1e9+7;
+const int N = 1e3 + 10;
+const ll MOD = 1e9 + 7;
 
-ll n, f[N][N];
+int n, f[N][N], ans = 0;
 string s;
 
-void solve() {
-    
+void solve(int i, int d, int bac) {
+    if(i > n) {
+        if(d == 0) {
+            ans = max(ans, bac);
+            f[i][d] = max(f[i][d], bac);
+        }
+        return;
+    }
+
+    if(f[i][d] != -1)
+        return;
+
+    f[i][d] = 0;
+    if(s[i] != ')' && d + 1 < n - i) {
+        solve(i + 1, d + 1, max(bac, d + 1));
+        f[i][d] = max(f[i][d], f[i + 1][d + 1]);
+    }
+    if(s[i] != '(' && d - 1 >= 0) {
+        solve(i + 1, d - 1, bac);
+        f[i][d] = max(f[i][d], f[i + 1][d - 1]);
+    }
 }
 
 void init() {
     cin >> s;
+    n = s.size();
+    s = ' ' + s;
+    memset(f, -1, sizeof(f));
+
+    solve(1, 0, 0);
+
+    cout << ans;
 }
 
 #define task "BTN1"
 int32_t main() {
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
-    if(fopen(task".inp","r")) {
-        freopen(task".inp","r",stdin);
-        freopen(task".out","w",stdout);
+    if(fopen(task ".inp", "r")) {
+        freopen(task ".inp", "r", stdin);
+        freopen(task ".out", "w", stdout);
     }
     int test_case = 1;
-    //cin >> test_case;
+    // cin >> test_case;
     while(test_case--) {
         init();
         // solve();
--- Dự tuyển/a.cpp
+++ Dự tuyển/a.cpp
@@ -1,41 +1,56 @@
-#include<bits/stdc++.h>
+/*--------------------POI--------------------*/
+#include <bits/stdc++.h>
 using namespace std;
-#define Nmax 1000001
-#define Tmax 10001
-#define str string
-#define endl '\n'
-#define fort(i,d,c) for(int i=d;i<=c;i++)
-#define forl(i,c,d) for(int i=c;i>=d;i--)
-#define fi first
-#define se second
-#define TASK N13
-string a , b;
-int n;
-long long f[20][2][2][2];
-long long cal(int i , bool oka , bool okb , bool pre)
-{
-    if(i >= n) return 1;
-    if(f[i][oka][okb][pre] != -1) return f[i][oka][okb][pre];
-    f[i][oka][okb][pre] = 0;
-    //cnt++;
-    for (int j = '0' ; j <= '9' ; j++)
-    {
-    if((j >= a[i] || oka) && (j <= b[i] || okb) && (j !='3' || !pre))
-    f[i][oka][okb][pre] += cal(i + 1 , oka || j > a[i] , okb || j < b[i] , j == '1');
-    }
-    return  f[i][oka][okb][pre];
+#define ll long long 
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define F first
+#define S second
+const ll inf = 0x3f3f3f3f3f3f3f3f;
+const int T = 1e9 + 7;
+
+void read_file(string file, bool read = 1, bool write = 1) {
+	string fileinp = file + ".INP", fileout = file + ".OUT";
+	const char* INP = fileinp.c_str();
+	const char* OUT = fileout.c_str();
+	if(read) freopen(INP, "r", stdin);
+	if(write) freopen(OUT, "w", stdout);
 }
-main(){
-    freopen("N13.INP","r",stdin);
-    freopen("N13.OUT","w",stdout);
-    while(cin >> a >> b)
-    {
-    memset(f , -1 , sizeof(f));
-    n = b.size();
-    while(a.size() != b.size())
-    {
-        a = '0' + a;
-    }
-    cout << cal(0 , false , false ,false) << endl;
-    }
+
+const int N = 1e3 + 5;
+string s;
+int n, ans = 0;
+ll dp[N][N];
+
+void Backtracking(int i, int d = 0, int max_d = 0) {
+	if(i == n) {
+		if(d == 0) {
+			ans = max(ans, max_d);
+			dp[i][d] = max(dp[i][d], (ll)max_d);
+		}
+		return ;
+	}
+	if(dp[i][d] != -1) return ;
+	dp[i][d] = 0;
+	if(s[i] ==  '(' || s[i] == '?') if(d <= n - i) {
+		Backtracking(i + 1, d + 1, max(max_d, d + 1));
+		dp[i][d] = max(dp[i][d], dp[i+1][d+1]);
+	}
+	if(s[i] == ')' || s[i] == '?') if(d > 0) {
+		Backtracking(i + 1, d - 1, max(max_d, d - 1));
+		dp[i][d] = max(dp[i][d], dp[i+1][d-1]);
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	read_file("BTN1");
+	memset(dp, -1, sizeof(dp));
+	cin >> s;
+	n = (int)s.size();
+	Backtracking(0);
+	cout << ans << "\n";
+	return 0;
 }
