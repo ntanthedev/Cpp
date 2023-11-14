5f6474fa9d670b3591ddefcacc69eabe64a08a3c
ntannn <nhattanff07@gmail.com>
2023-11-14 15:35:30 +0700

    ị

--- Du Tuyen/More/trangsach.cpp
+++ Du Tuyen/More/trangsach.cpp
@@ -20,8 +20,8 @@ void solve(){
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    freopen("trangsach.inp","r",stdin);
-    freopen("trangsach.out","w",stdout);
+    // freopen("trangsach.inp","r",stdin);
+    // freopen("trangsach.out","w",stdout);
     cin >> n >> a;
     solve();
 }
---
+++ Du Tuyen/QHD/KSACK1.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops,no-stack-protector")
+#pragma GCC target("sse4,avx2,fma")
+#define w first
+#define v second
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define all(x) x.begin(), x.end()
+#define TIME (1.0 * clock() / CLOCKS_PER_SEC)
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+
+const int N = 1e6+10;
+const ll MOD = 1e9+7;
+
+template <typename T> inline void read (T &x) {
+    bool b = 0;
+    char c;
+    while (!isdigit (c = getchar()) && c != '-');
+    if (c == '-') {
+        c = getchar();
+        b = 1;
+    }
+    x = c - 48;
+    while (isdigit(c = getchar())) {
+        x = (x << 3) + (x << 1) + (c - 48);
+    }
+    if (b) {
+        x=-x;
+    }
+}
+
+ll n, m, gmax = 0, s[101];
+pll a[101];
+vll ans, res;
+
+void solve(int i, ll kl, ll gt) {
+    if(i > n) {
+        if(gt > gmax) {
+            ans = res;
+            gmax = gt;
+        }
+        return;
+    }
+    for(int j = 0; j <= 1; ++j) {
+        if(kl + j * a[i].w > m || gt + j * a[i].v + (s[n] - s[i]) <= gmax)
+            continue;
+        if(j)
+            res.eb(i);
+        solve(i + 1, kl + j * a[i].w, gt + j * a[i].v);
+        if(j)
+            res.pop_back();
+    }
+}
+
+void init() {
+    read(n), read(m);
+    s[0] = 0;
+    for(int i = 1; i <= n; i++) {
+        read(a[i].w), read(a[i].v);
+        s[i] = s[i-1] + a[i].v;
+    }
+    solve(1, 0, 0);
+    cout << gmax << '\n';
+    for(auto i : ans)
+        cout << i << " " ;
+}
+
+#define task "KSACK1"
+signed main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    if(fopen(task".inp","r")) {
+        freopen(task".inp","r",stdin);
+        freopen(task".out","w",stdout);
+    }
+    int test_case = 1;
+    //cin >> test_case;
+    while(test_case--) {
+        init();
+        // solve();
+    }
+    //cerr << '\n' << "\x1b[31mtime is: " << TIME << "\e[39m";
+}
