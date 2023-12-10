44ce2fd9fecb9e788c71bfc809739ee873350bfc
ntannn <nhattanff07@gmail.com>
2023-12-10 17:43:34 +0700

    a

--- Du Tuyen/a.cpp
+++ Du Tuyen/a.cpp
@@ -1,58 +1,28 @@
 #include<bits/stdc++.h>
-//#pragma GCC optimize("O3,unroll-loops,no-stack-protector")
-//#pragma GCC target("sse4,avx2,fma")
-#define fi first
-#define se second
-#define mp make_pair
-#define pb push_back
-#define eb emplace_back
-#define all(x) x.begin(), x.end()
-#define TIME (1.0 * clock() / CLOCKS_PER_SEC)
+#define int long long
 
 using namespace std;
 
-typedef long long ll;
-typedef pair<int,int> ii;
-typedef pair<ll,ll> pll;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef vector<ii> vii;
-typedef vector<vi> vvi;
-
-const int N = 1e6+10;
-const ll MOD = 1e9+7;
-
-ll n, k, a[N], ans = 0;
-
-void solve() {
-    for(ll i = 1; i <= n; i++) {
-        for(ll j = i-1; j >= max(i - k, 0LL); j--)
-            ans = max(ans, a[i] - a[j]);
+signed main() {
+    int n, x, ans = 0;
+    map<int, int> M;
+    stack<int> st;
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        cin >> x;
+        while(!st.empty() && x > st.top()) {
+            M[st.top()]--;
+            st.pop();
+            ans++;
+        }
+        if(!st.empty()) {
+            if(st.top() == x)
+                ans += M[x] + (st.size() > M[x]);
+            else
+                ans++; 
+        }
+        st.push(x);
+        M[x]++;
     }
     cout << ans;
-}
-
-void init() {
-    cin >> n >> k;
-    for(int i = 1; i <= n; i++) {
-        cin >> a[i];
-        a[i] += a[i-1];
-    }
-}
-
-#define task "a"
-signed main() {
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
-    if(fopen(task".inp","r")) {
-        freopen(task".inp","r",stdin);
-        freopen(task".out","w",stdout);
-    }
-    int test_case = 1;
-    //cin >> test_case;
-    while(test_case--) {
-        init();
-        solve();
-    }
-    //cerr << '\n' << "\x1b[31mtime is: " << TIME << "\e[39m";
 }
--- Đề/Đề luyện thầy Dũng/Segment Tree/QMAX2.cpp
+++ Đề/Đề luyện thầy Dũng/Segment Tree/QMAX2.cpp
@@ -1,75 +1,65 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define int long long
-const int N=5e4+10;
+const int N = 5e4 + 10;
 #define M 1e18
 using namespace std;
-int n,m,a[N],l[4*N],h[4*N],maxn[4*N],s1,x,y,z,lazy[4*N];
-void update(int x, int a, int b, int i, int j, int k)
-{
-    if(lazy[x]!=0)
-    {
-        maxn[x]+=lazy[x];
-        if(a!=b)
-        {
-            lazy[2*x]+=lazy[x];
-            lazy[2*x+1]+=lazy[x];
+int n, m, a[N], l[4 * N], h[4 * N], maxn[4 * N], s1, x, y, z, lazy[4 * N];
+void update(int x, int a, int b, int i, int j, int k) {
+    if(lazy[x] != 0) {
+        maxn[x] += lazy[x];
+        if(a != b) {
+            lazy[2 * x] += lazy[x];
+            lazy[2 * x + 1] += lazy[x];
         }
-        lazy[x]=0;
+        lazy[x] = 0;
     }
-    if(a>b || a>j || b<i) return ;
-    if(a>=i && b<=j)
-    {
-        maxn[x]+=k;
-        if(a!=b)
-        {
-            lazy[2*x]+=k;
-            lazy[2*x+1]+=k;
+    if(a > b || a > j || b < i)
+        return;
+    if(a >= i && b <= j) {
+        maxn[x] += k;
+        if(a != b) {
+            lazy[2 * x] += k;
+            lazy[2 * x + 1] += k;
         }
-        return ;
+        return;
     }
-    int mid=(a+b)/2;
-    update(2*x,a,mid,i,j,k);
-    update(2*x+1,mid+1,b,i,j,k);
-    maxn[x]=max(maxn[2*x],maxn[2*x+1]);
+    int mid = (a + b) / 2;
+    update(2 * x, a, mid, i, j, k);
+    update(2 * x + 1, mid + 1, b, i, j, k);
+    maxn[x] = max(maxn[2 * x], maxn[2 * x + 1]);
 }
-int query(int x,int a,int b,int i, int j)
-{
-    if(a>b || a>j || b<i) return (-M);
-    if(lazy[x]!=0)
-    {
-        maxn[x]+=lazy[x];
-        if(a!=b)
-        {
-            lazy[2*x]+=lazy[x];
-            lazy[2*x+1]+=lazy[x];
+int query(int x, int a, int b, int i, int j) {
+    if(a > b || a > j || b < i)
+        return (-M);
+    if(lazy[x] != 0) {
+        maxn[x] += lazy[x];
+        if(a != b) {
+            lazy[2 * x] += lazy[x];
+            lazy[2 * x + 1] += lazy[x];
         }
-        lazy[x]=0;
+        lazy[x] = 0;
     }
-    if(a>=i && b<=j) return maxn[x];
-    int q1=query(2*x,a,(a+b)/2,i,j);
-    int q2=query(2*x+1,(a+b)/2+1,b,i,j);
-    return max(q1,q2);
+    if(a >= i && b <= j)
+        return maxn[x];
+    int q1 = query(2 * x, a, (a + b) / 2, i, j);
+    int q2 = query(2 * x + 1, (a + b) / 2 + 1, b, i, j);
+    return max(q1, q2);
 }
-main()
-{
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    freopen("QMAX2.INP","r",stdin);
-    freopen("QMAX2.OUT","w",stdout);
-    cin>>n>>m;
-    memset(lazy,0,sizeof(lazy));
-    for(int i=1;i<=m;i++)
-    {
-        cin>>s1;
-        if(s1==0)
-        {
-            cin>>x>>y>>z;
-            update(1,1,n,x,y,z);
-        }
-        else
-        {
-            cin>>x>>y;
-            cout<<query(1,1,n,x,y)<<"\n";
+    freopen("QMAX2.INP", "r", stdin);
+    freopen("QMAX2.OUT", "w", stdout);
+    cin >> n >> m;
+    memset(lazy, 0, sizeof(lazy));
+    for(int i = 1; i <= m; i++) {
+        cin >> s1;
+        if(s1 == 0) {
+            cin >> x >> y >> z;
+            update(1, 1, n, x, y, z);
+        } else {
+            cin >> x >> y;
+            cout << query(1, 1, n, x, y) << "\n";
         }
     }
 }
