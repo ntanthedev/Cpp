df74b5b0be6290bf1054ada53ea8d30eaa602277
ntannn <nhattanff07@gmail.com>
2023-12-14 09:57:49 +0700

    ok

--- Du Tuyen/CSES/Hotel Queries.cpp
+++ Du Tuyen/CSES/Hotel Queries.cpp
@@ -19,32 +19,20 @@ typedef vector<ll> vll;
 typedef vector<ii> vii;
 typedef vector<vi> vvi;
 
-const int N = 1e3+10;
+const int N = 1e6+10;
 const ll MOD = 1e9+7;
 
-ll n, Q;
-int a[N][N], s[N][N];
-char c;
+
 
 void solve() {
-    ii x, y;
-    while(Q--) {
-        cin >> x.fi >> x.se >> y.fi >> y.se;
-        cout << a[y.fi][y.se] - a[x.fi-1][y.se] - a[y.fi][x.se-1] + a[x.fi-1][x.se-1] << '\n';
-    }
+
 }
 
 void init() {
-    cin >> n >> Q; 
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= n; j++) {
-            cin >> c;
-            a[i][j] = a[i][j-1] + a[i-1][j] - a[i-1][j-1] + (c == '*');
-        }
-    }
+
 }
 
-#define task "CSES - Forest Queries "
+#define task "Hotel Queries"
 signed main() {
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
