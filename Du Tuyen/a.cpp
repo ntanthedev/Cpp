3b2a21b9751847c51952dbcd355f2f516d297028
ntannn <nhattanff07@gmail.com>
2023-12-14 16:05:17 +0700

    .

--- Du Tuyen/a.cpp
+++ Du Tuyen/a.cpp
@@ -1,28 +1,34 @@
 #include <bits/stdc++.h>
-#define int long long
-
 using namespace std;
-
-signed main() {
-    int n, x, ans = 0;
-    map<int, int> M;
-    stack<int> st;
+int n, q, t;
+int a[100001], ma[100001];
+long long f[100001], g[100001];
+void input() {
     cin >> n;
-    for(int i = 1; i <= n; i++) {
-        cin >> x;
-        while(!st.empty() && x > st.top()) {
-            M[st.top()]--;
-            st.pop();
-            ans++;
-        }
-        if(!st.empty()) {
-            if(st.top() == x)
-                ans += M[x] + (st.size() > M[x]);
-            else
-                ans++;
-        }
-        st.push(x);
-        M[x]++;
+    for(int i = 1; i <= n; i++)
+        cin >> a[i];
+}
+void solve() {
+    ma[n] = a[n];
+    f[n] = 0;
+    g[n] = 0;
+    for(int i = n - 1; i >= 1; i--) {
+        ma[i] = max(a[i], ma[i + 1]);
+        f[i] = ma[i] - a[i];
+        g[i] = f[i] + g[i + 1];
     }
-    cout << ans;
+    cin >> q;
+    for(int i = 1; i <= q; i++) {
+        cin >> t;
+        cout << g[t] << endl;
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    freopen("vni.inp", "r", stdin);
+    freopen("vni.out", "w", stdout);
+    input();
+    solve();
 }
