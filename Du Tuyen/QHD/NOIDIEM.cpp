7ce932ad78d47d018227b4d60253ca9095d0b795
ntannn <nhattanff07@gmail.com>
2023-12-25 08:38:49 +0700

    ok

--- Du Tuyen/QHD/NOIDIEM.cpp
+++ Du Tuyen/QHD/NOIDIEM.cpp
@@ -1,50 +1,33 @@
-#include<bits/stdc++.h>
-//#pragma GCC optimize("O3,unroll-loops,no-stack-protector")
-//#pragma GCC target("sse4,avx2,fma")
-#define fi first
-#define se second
-#define mp make_pair
-#define pb push_back
-#define eb emplace_back
-#define all(x) x.begin(), x.end()
-#define TIME (1.0 * clock() / CLOCKS_PER_SEC)
+#include <bits/stdc++.h>
 
 using namespace std;
-
 typedef long long ll;
-typedef pair<int,int> ii;
-typedef pair<ll,ll> pll;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef vector<ii> vii;
-typedef vector<vi> vvi;
-
-const int N = 1e3+10;
-const ll MOD = 1e9+7;
-
-ll n, m, f[N][N];
-
 void solve() {
-
+    // initialize
+    ll n, k, S = 0;
+    cin >> n >> k;
+    vector<ll> a(n + 1);
+    vector<vector<ll>> dp(n + 1, vector<ll>(n + 1));
+    for(ll i = 1; i <= n; i++)
+        cin >> a[i];
+    for(ll i = 1; i <= n; i++) {
+        for(ll j = 1; j <= n; j++) {
+            dp[i][j] = max(dp[i][j], dp[i - 1][j - 1]);
+            if(abs(a[j] - i) <= k)
+                dp[i][j]++;
+            dp[i][j] = max({dp[i][j], dp[i][j - 1], dp[i - 1][j]});
+        }
+    }
+    cout << dp[n][n];
 }
 
-void init() {
-
-}
-
-#define task "NOIDIEM"
 signed main() {
+    ios::sync_with_stdio(false);
     cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
-    if(fopen(task".inp","r")) {
-        freopen(task".inp","r",stdin);
-        freopen(task".out","w",stdout);
+    if(fopen("NOIDIEM.inp", "r")) {
+        freopen("NOIDIEM.inp", "r", stdin);
+        freopen("NOIDIEM.out", "w", stdout);
     }
-    int test_case = 1;
-    //cin >> test_case;
-    while(test_case--) {
-        init();
-        solve();
-    }
-    //cerr << '\n' << "\x1b[31mtime is: " << TIME << "\e[39m";
+    solve();
+    //---writer---
 }
