6cc26cb98291b28173692e5b1afe28418851531d
ntannn <nhattanff07@gmail.com>
2024-01-06 17:19:34 +0700

    ok

--- Du Tuyen/a.cpp
+++ Du Tuyen/a.cpp
@@ -1,39 +1,66 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+//#pragma GCC optimize("O3,unroll-loops,no-stack-protector")
+//#pragma GCC target("sse4,avx2,fma")
 #define fi first
 #define se second
-typedef long long ll;
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define all(x) x.begin(), x.end()
+#define TIME (1.0 * clock() / CLOCKS_PER_SEC)
+
 using namespace std;
-const ll N = 2e2 + 21;
-const ll NN = 5e2 + 21;
-ll a[N][N], ans = 0, n;
-bool vis[N][N][NN];
-pair<ll, ll> fa[N][N];
-void dfs(ll x, ll y, ll p) {
-    vis[x][y][p] = true;
-    if(x == n && y == n)
-        ans = max(ans, p);
-    if(x > 1 && !vis[x - 1][y][p ^ a[x - 1][y]])
-        dfs(x - 1, y, p ^ a[x - 1][y]);
-    if(y > 1 && !vis[x][y - 1][p ^ a[x][y - 1]])
-        dfs(x, y - 1, p ^ a[x][y - 1]);
-    if(x < n && !vis[x + 1][y][p ^ a[x + 1][y]])
-        dfs(x + 1, y, p ^ a[x + 1][y]);
-    if(y < n && !vis[x][y + 1][p ^ a[x][y + 1]])
-        dfs(x, y + 1, p ^ a[x][y + 1]);
-}
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+
+const int N = 1e3+10;
+const ll MOD = 1e9+7;
+
+ll n, k, lens;
+ll a[N][N];
+
 void solve() {
-    cin >> n;
-    for(ll i = 1; i <= n; i++) {
-        for(ll j = 1; j <= n; j++)
-            cin >> a[i][j];
+    lens = n * n;
+    for(int i = 1; i <= n; i++) {
+        for(int j = n; j >= k; j--) {
+            a[i][j] = lens--;
+        }
     }
-    dfs(1 * 1ll, 1 * 1ll, a[1][1]);
-    cout << ans;
+    for(int i = 1; i <= n; i++) {
+        for(int j = k-1; j >= 1; j--) {
+            a[i][j] = lens--;
+        }
+    }
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) 
+            cout << a[i][j] << " " ;
+        cout << '\n';
+    }
 }
 
-signed main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+void init() {
+    cin >> n >> k;
+}
 
-    solve();
+#define task "a"
+signed main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    if(fopen(task".inp","r")) {
+        freopen(task".inp","r",stdin);
+        freopen(task".out","w",stdout);
+    }
+    int test_case = 1;
+    //cin >> test_case;
+    while(test_case--) {
+        init();
+        solve();
+    }
+    //cerr << '\n' << "\x1b[31mtime is: " << TIME << "\e[39m";
 }
--- VNOI/code/origin/code.cpp
+++ VNOI/code/origin/code.cpp
@@ -1,63 +1,44 @@
-#include<bits/stdc++.h>
-//#pragma GCC optimize("O3,unroll-loops,no-stack-protector")
-//#pragma GCC target("sse4,avx2,fma")
+#include <bits/stdc++.h>
 #define fi first
 #define se second
-#define mp make_pair
-#define pb push_back
-#define eb emplace_back
-#define all(x) x.begin(), x.end()
-#define TIME (1.0 * clock() / CLOCKS_PER_SEC)
-
-using namespace std;
-
 typedef long long ll;
-typedef pair<int,int> ii;
-typedef pair<ll,ll> pll;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef vector<ii> vii;
-typedef vector<vi> vvi;
-
-const int N = 1e6+10;
-const ll MOD = 1e9+7;
-
-ll n, k, d;
-string s;
-
+using namespace std;
+const ll N = 2e2 + 21;
+const ll NN = 5e2 + 21;
+ll a[N][N], ans = 0, n;
+bool vis[N][N][NN];
+pair<ll, ll> fa[N][N];
+void dfs(ll x, ll y, ll p) {
+    vis[x][y][p] = true;
+    if(x == n && y == n)
+        ans = max(ans, p);
+    if(x > 1 && !vis[x - 1][y][p ^ a[x - 1][y]])
+        dfs(x - 1, y, p ^ a[x - 1][y]);
+    if(y > 1 && !vis[x][y - 1][p ^ a[x][y - 1]])
+        dfs(x, y - 1, p ^ a[x][y - 1]);
+    if(x < n && !vis[x + 1][y][p ^ a[x + 1][y]])
+        dfs(x + 1, y, p ^ a[x + 1][y]);
+    if(y < n && !vis[x][y + 1][p ^ a[x][y + 1]])
+        dfs(x, y + 1, p ^ a[x][y + 1]);
+}
 void solve() {
-    s.resize(k, '1');
-    n -= k;
-    for(int i = 0; i < s.size(); i++) {
-        if(n > 8) {
-            s[i] = '9';
-            n -= 8;
-        }
-        else {
-            s[i] += n;
-            break;
-        }
+    cin >> n;
+    for(ll i = 1; i <= n; i++) {
+        for(ll j = 1; j <= n; j++)
+            cin >> a[i][j];
     }
-    cout << s;
+    dfs(1 * 1ll, 1 * 1ll, a[1][1]);
+    cout << ans;
 }
 
-void init() {
-    cin >> n >> k;
-}
-
-#define task "code"
 signed main() {
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
+    #define task "code"
     if(fopen(task".inp","r")) {
         freopen(task".inp","r",stdin);
         freopen(task".out","w",stdout);
     }
-    int test_case = 1;
-    //cin >> test_case;
-    while(test_case--) {
-        init();
-        solve();
-    }
-    //cerr << '\n' << "\x1b[31mtime is: " << TIME << "\e[39m";
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
 }
