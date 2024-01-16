dd1bb348142d5cb778bfc083e2db85e5c3020b1d
ntannn <nhattanff07@gmail.com>
2024-01-16 15:32:38 +0700

    ok

--- Du Tuyen/QHD/EXPRESSION.cpp
+++ Du Tuyen/QHD/EXPRESSION.cpp
@@ -23,28 +23,28 @@ const int N = 1e6+10;
 const ll MOD = 1e9+7;
 
 ll n, S, a[N], s[N];
-vll ans;
 bool flag = 0;
+string ans;
 
-void solve(int i = 1, ll res = 0) {
+void dynamic_programing() {
+
+}
+
+void solve(int i = 2, ll res = a[1]) {
     if(flag)
         return;
     if(i > n) {
         if(res == S) {
-            cout << "YES\n";
-            for(auto j : ans)
-                cout << j << " " ;
+            cout << ans;
             flag = 1;
         }
     }
-    for(int j = 0; j <= 1; j++) {
-        if(res + a[i]*j > S || res + (s[n] - s[i]) + j*a[i] < S)
+    for(int j = -1; j <= 1; j += 2) {
+        if(res + a[i]*j - (s[n] - s[i]) > S || res + j*a[i] + (s[n] - s[i]) < S)
             continue;
-        if(j)
-            ans.pb(i);
-        solve(i+1, res + a[i]*j);
-        if(j)
-            ans.pop_back();
+        ans.pb((j == -1 ? '-' : '+'));
+        solve(i+1, res + j*a[i]);
+        ans.pop_back();
     }
 }
 
@@ -57,7 +57,7 @@ void init() {
     }
 }
 
-#define task "SUBSETSUM"
+#define task "EXPRESSION"
 signed main() {
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
@@ -70,8 +70,6 @@ signed main() {
     while(test_case--) {
         init();
         solve();
-        if(!flag)
-            cout << "NO";
     }
     //cerr << '\n' << "\x1b[31mtime is: " << TIME << "\e[39m";
 }
@@ -84,6 +82,7 @@ using namespace std;
 const int N = 110;
 
 int n, m, a[N], sum = 0, dp[10000009];
+bool mark[N];
 
 void ReadInput() {
     cin >> n >> m;
@@ -93,9 +92,11 @@ void ReadInput() {
     }    
 }
 void solve() {
+    sum -= m;
+    sum /= 2;
     vector<vector<int>> res(sum + 1);
     dp[0] = 1;
-    for (int i = 1; i <= n; ++i) {
+    for (int i = 2; i <= n; ++i) {
         for (int j = sum; j >= a[i]; --j) {
             if (dp[j - a[i]]) {
                 dp[j] += dp[j - a[i]];
@@ -104,24 +105,40 @@ void solve() {
             }
         }
     }
-    if (!dp[m]) {
-        cout << "NO";
-        return;
+    for (auto it : res[sum]) {
+        mark[it] = 1;
     }
-    cout << "YES" << '\n';
-    //dp[3][2] dp[2][1]
-    for (auto it : res[m]) {
-        cout << it <<" ";
+    for (int i = 2; i <= n; ++i) {
+        if (mark[i]) cout << "-";
+        else cout << "+";
     }
 }
 int main() {
 
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
-    freopen("SUBSETSUM.inp", "r", stdin);
-    freopen("SUBSETSUM.out", "w", stdout);
+    freopen("EXPRESSION.inp", "r", stdin);
+    freopen("EXPRESSION.out", "w", stdout);
     ReadInput();
     solve();
 
 }
+
+// 8 - 7  - 4  + 3  + 5
+
+// //8 3 5
+// 16 
+// 26
+// 10
+
+
+
+// 8 - 7 + 9 + 7 + 4  - 3
+
+
+// 18
+
+// 38 
+
+// 20
 */
