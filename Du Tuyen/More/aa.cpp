5242148e2d65cbc6ff52503a29837bf8a9a2e41e
ntannn <nhattanff07@gmail.com>
2024-06-18 12:26:44 +0700

    ok

--- Du Tuyen/More/aa.cpp
+++ Du Tuyen/More/aa.cpp
@@ -1,68 +1,76 @@
+//problem "b"
+//created in 23:43:04 - Sun 16/06/2024
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define int int64_t
+
 using namespace std;
-#define fi first
-#define se second
-typedef long long ll;
+using namespace __gnu_pbds;
 
-void solve() {
-    ll n;
-    cin>>n;
-    string s;
-    cin>>s;
-    s=' '+s;
-    vector<ll>save;
-    save.push_back(0LL);
-    unordered_map<string,ll>M;
-    ll d=0,dd=0;
-    string t,emp;
-    for(ll i=1;i<=n;i++)
-    {
-        if(s[i]=='(') d++;
-        else d--;
-        t.push_back(s[i]);
-        if(!d)
-        {
-            if(!M[t])
-            {
-                dd++;
-                M[t]=dd;
-                save.push_back(dd);
-            }
-            else save.push_back(M[t]);
-            t=emp;
-        }
-    }
-    n=save.size();
-    n--;
-    for(ll i=1;i<=n;i++)
-    {
-        if(n%i==0)
-        {
-            ll sas=2;
-            for(ll j=1;j<=i;j++)
-            {
-                for(ll k=j+i+1;k<=n;k+=i)
-                {
-                    if(save[k]!=save[j])
-                    {
-                        sas--;
-                    }
-                }
-            }
-            if(sas>0)
-            {
-                
-            }
-        }
-    }
-}
-int main() {
-    ios_base::sync_with_stdio(false);
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+signed main() {
+    ios::sync_with_stdio(false);
     cin.tie(NULL);
-    ll tests=1;
-    while(tests--) {
-        solve();
+
+    int n;
+    cin >> n;
+    n *= 2;
+    int a[n];
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    vector<pair<int, int>> v;
+    int c = 1;
+    int mn = a[0];
+    for(int i = 1; i < n; i++) {
+        if(a[i] < mn)
+            c++;
+        else {
+            v.push_back({i - c, c});
+            c = 1;
+            mn = a[i];
+        }
+    }
+
+    v.push_back({n - c, c});
+    int dp[n / 2 + 1];
+    memset(dp, -1, sizeof(dp));
+    dp[0] = -2;
+    for(int i = 0; i < v.size(); i++) {
+        for(int j = n / 2; j >= v[i].second; j--) {
+            if(dp[j - v[i].second] != -1 && dp[j] == -1)
+                dp[j] = i;
+        }
+    }
+    if(dp[n / 2] == -1)
+        cout << -1 << endl;
+    else {
+        int ans[n / 2];
+        set<int> s;
+        int j = n / 2;
+        int i = n - 1;
+        int l = n / 2;
+        while(j > 0) {
+            int f = v[dp[j]].first, s = v[dp[j]].second;
+            c = l - s;
+            l -= s;
+            i = f;
+            while(s--) {
+                ans[c++] = a[i++];
+            }
+            j -= v[dp[j]].second;
+        }
+        for(auto x: ans) {
+            cout << x << " ";
+            s.insert(x);
+        }
+        cout << endl;
+        for(int i = 0; i < n; i++) {
+            if(s.find(a[i]) == s.end())
+                cout << a[i] << " ";
+        }
+        cout << endl;
     }
 }
-
-//for(int i = 1; i <= n; i++) cin >> a[i] >> a[j];
--- Du Tuyen/More/b.cpp
+++ Du Tuyen/More/b.cpp
@@ -1,137 +1,14 @@
-#include <bits/stdc++.h>
+//problem "b"
+//created in 00:48:45 - Mon 17/06/2024
+
+#include<bits/stdc++.h>
+
+#define int int64_t
+
 using namespace std;
-#define fi first
-#define se second
-typedef long long ll;
-void solve() {
-    ll n;
-    cin >> n;
-    bool o = false;
-    vector<ll> c(2 * n + 1);
-    for(ll i = 1; i <= 2 * n; i++)
-        cin >> c[i];
-    for(ll i = 0; i <= n; i++) {
-        deque<ll> a, b;
-        for(ll j = 2 * n; j >= 2 * n - i + 1; j--)
-            b.push_front(c[j]);
-        ll flag = 2;
-        for(ll j = 2 * n - i; j >= 1; j--) {
-            if(j == (2 * n - i)) {
-                a.push_front(c[j]);
-                flag = 1;
-            } else if(b.size() == n) {
-                a.push_front(c[j]);
-                flag = 1;
-            } else if(a.size() == n) {
-                b.push_front(c[j]);
-                flag = 2;
-            } else {
-                if(c[j] > c[j + 1]) {
-                    if(flag == 2)
-                        b.push_front(c[j]);
-                    else
-                        a.push_front(c[j]);
-                } else {
-                    if(flag == 2) {
-                        a.push_front(c[j]);
-                        flag = 1;
-                    } else {
-                        b.push_front(c[j]);
-                        flag = 2;
-                    }
-                }
-            }
-        }
-        vector<ll> check(2 * n + 1);
-        ll a1 = 0, b1 = 0, c1 = 1;
-        while(a1 < n && b1 < n) {
-            if(a[a1] < b[b1]) {
-                check[c1] = a[a1], a1++, c1++;
-            } else {
-                check[c1] = b[b1], b1++, c1++;
-            }
-        }
-        while(a1 < n) {
-            check[c1] = a[a1], a1++, c1++;
-        }
-        while(b1 < n) {
-            check[c1] = b[b1], b1++, c1++;
-        }
-        if(check == c) {
-            for(auto i: a)
-                cout << i << " ";
-            cout << '\n';
-            for(auto i: b)
-                cout << i << " ";
-            o = true;
-            return;
-        }
-    }
-    for(ll i = 0; i <= n; i++) {
-        deque<ll> a, b;
-        for(ll j = 2 * n; j >= 2 * n - i + 1; j--)
-            a.push_front(c[j]);
-        ll flag = 1;
-        for(ll j = 2 * n - i; j >= 1; j--) {
-            if(j == (2 * n - i)) {
-                b.push_front(c[j]);
-                flag = 2;
-            } else if(b.size() == n) {
-                a.push_front(c[j]);
-                flag = 1;
-            } else if(a.size() == n) {
-                b.push_front(c[j]);
-                flag = 2;
-            } else {
-                if(c[j] > c[j + 1]) {
-                    if(flag == 2)
-                        b.push_front(c[j]);
-                    else
-                        a.push_front(c[j]);
-                } else {
-                    if(flag == 2) {
-                        a.push_front(c[j]);
-                        flag = 1;
-                    } else {
-                        b.push_front(c[j]);
-                        flag = 2;
-                    }
-                }
-            }
-        }
-        vector<ll> check(2 * n + 1);
-        ll a1 = 0, b1 = 0, c1 = 1;
-        while(a1 < n && b1 < n) {
-            if(a[a1] < b[b1]) {
-                check[c1] = a[a1], a1++, c1++;
-            } else {
-                check[c1] = b[b1], b1++, c1++;
-            }
-        }
-        while(a1 < n) {
-            check[c1] = a[a1], a1++, c1++;
-        }
-        while(b1 < n) {
-            check[c1] = b[b1], b1++, c1++;
-        }
-        if(check == c) {
-            for(auto i: a)
-                cout << i << " ";
-            cout << '\n';
-            for(auto i: b)
-                cout << i << " ";
-            o = true;
-            return;
-        }
-    }
-    if(!o)
-        cout << -1 << '\n' << -1;
-}
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    ll tests = 1;
-    while(tests--) {
-        solve();
-    }
+
+int32_t main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    
+    
 }
