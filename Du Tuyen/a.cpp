baa21d4836deb29713bb659432d2523acbc905a7
ntannn <nhattanff07@gmail.com>
2024-07-20 08:10:57 +0700

    o

--- Du Tuyen/a.cpp
+++ Du Tuyen/a.cpp
@@ -1,37 +1,107 @@
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-#define ld long double 
-const long long MOD = 1e9+7;
-const long long N = 1e6+9;
-ll n, a[N], x, cnt = 0;
-stack<ll>st;
-void solve(){    
-    cin>>a[1];
-    st.push(1);
-    for(int i = 2; i <= n; i++){
-        cin>>a[i];
-        ll gmax = a[st.top()] - 1;
-        while(!st.empty() && (a[st.top()] < a[i] && a[st.top()] > gmax)){
-            st.pop();
-            cnt++;
-            gmax = a[st.top()];
-        }
-        if(!st.empty())
-            cnt++;
-        while(!st.empty() && a[st.top()] == a[i])
-            st.pop();
-        st.push(i);
-    }
-    cout<<cnt;
-}
+//Author : Trieu self-destruct
+//1st DHBB
+//1st O30/4 (11 :<)
+//TST next year
 
-int main()
-{
-    cin.tie(NULL); cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
-    // freopen("PAIRS.inp","r",stdin);
-    // freopen("PAIRS.out","w",stdout);
-    cin>>n;
-    solve();
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+
+//using namespace __gnu_pbds;
+
+using namespace std;
+
+/*Pragma*/
+#pragma GCC optimize("Ofast")
+//#pragma GCC optimize("O3")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+//#pragma GCC optimize("Os")
+
+/*Weird define*/
+#define ll long long
+//#define endl '\n'
+#define gcd __gcd
+//#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+//#define p prev
+#define count1 __builtin_popcount
+
+/*For define*/
+#define For(i,a,b) for(ll i=a;i<=b;i++)
+#define foR(i,a,b) for(ll i=a;i>=b;i--)
+#define FOR(i,a,b) for(ll i=a;i<b;i++)
+#define forN(i,a,b,c) for(ll i=a;i<=b;i+=c)
+#define foRN(i,a,b,c) for(ll i=a;i>=b;i-=c)
+#define fora(i, a) for (auto i : a)
+
+/*push-pop-pair define*/
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define x first
+#define y second
+
+/*Print define*/
+#define write(a, l, r) for (ll ik = l; ik <= r; ik++) cout << a[ik] << ' '; cout << endl
+#define writeln(a, l, r) for (ll ik = l; ik <= r; ik++) cout << a[ik] << endl
+
+/*Type-def*/
+typedef unsigned long long 	ull;
+typedef vector<ll> 		vi;
+typedef pair<ll, ll>		ii;
+typedef long double         ld;
+
+/*some const*/
+const ll IMA = LLONG_MAX-10;
+const ll IMI = LLONG_MIN+10;
+const ll INF = INT_MAX;
+const ll MOD = 1e9+7;
+
+/* Calculation with MODULO*/
+ll dec(ll x, ll y) {return (x-y+MOD)%MOD;}
+ll add(ll x, ll y) {return (x + y) % MOD;}
+ll mul(ll x, ll y) {return ((x%MOD) * (y%MOD)) % MOD;}
+ll bpow(ll x, ll y) {ll res = 1; while (y) {if (y & 1) res = mul(res, x); x = mul(x, x); y >>= 1;} return res;}
+ll ModInverse(ll x) {return bpow(x, MOD - 2);}
+ll Div(ll x, ll y) {return mul(x, ModInverse(y));}
+ll GetBit(ll val, ll num) {return ((val>>num) & 1LL);}
+
+/*Some simple checking and calculation function*/
+string yn(bool k){if (k) return "YES"; else return "NO";}
+ll binpower(ll base, ll e, ll mod) {ll result = 1;base %= mod;while (e){if (e & 1)result = (ll)result * base % mod;base = (ll)base * base % mod;e >>= 1;    }return result;}
+bool check_composite(long long n, long long a, long long d, int s) {long long x = binpower(a, d, n); if (x == 1 or x == n - 1)return false;for (int r = 1; r < s; r++) {x = (long long)x * x % n;if (x == n - 1)return false;}return true;};
+bool checkprime(long long n) {if (n < 2)return false;int r = 0;long long d = n - 1;while ((d & 1) == 0) {d >>= 1;r++;}for (int a : {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}) {if (n == a)return true;if (check_composite(n, a, d, r))return false;}return true;}
+bool checksqr(ll k){return (trunc(sqrt(k))*trunc(sqrt(k))==k);}
+ll lcm(ll a, ll b){return (a*b)/gcd(a,b);}
+
+const int N=2e5+100;
+ll f[N];
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    //freopen("GRAPHDIV.INP","r",stdin);
+    //freopen("GRAPHDIV.OUT","w",stdout);
+    ll n,m;
+    cin >> n >> m;
+    f[0]=f[1]=1;
+    For(i,2,N) f[i]=(f[i-1]+f[i-2])%MOD;
+    cout << mul(2,dec(add(f[n],f[m]),1));
 }
+/*
+//Delete define endl when interactive prolem
+
+INPUT :
+4 1
+1 1 1 2
+
+OUTPUT :
+
+If it WA, check :
+- Special case (Usually, n=1)
+- WRONG FORMAT OUTPUT
+- Check reading
+- Change (ll) to (ull)
+- lleger Overflow (The number that bigger than 2^63-1)
+*/
