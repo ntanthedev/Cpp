f8693c9f12bd46c736264ca368055a8b1b2dea30
ntannn <nhattanff07@gmail.com>
2024-07-21 12:48:07 +0700

    ok

--- Du Tuyen/More/b.cpp
+++ Du Tuyen/More/b.cpp
@@ -1,22 +1,14 @@
 //problem "b"
-//created in 00:11:56 - Wed 17/07/2024
+//created in 10:06:15 - Sat 20/07/2024
 
-#include<bits/stdc++.h> 
- 
-#define int int64_t 
-const int MOD = 1e9 + 7;
- 
-using namespace std; 
- 
-int32_t main() { 
-    ios_base::sync_with_stdio(false); cin.tie(NULL); 
-      
-    int n, m;
-    cin >> m >> n;
-    vector<int> f(max(m + 1, n + 1));
-    f[0] = 1,
-    f[1] = 1;
-    for(int i = 2; i <= max(m, n); ++i) 
-        f[i] = (f[i - 1] + f[i - 2]) % MOD;
-    cout << (2 * (f[n] + f[m] - 1) % MOD) % MOD;
-} 
+#include<bits/stdc++.h>
+
+#define int int64_t
+
+using namespace std;
+
+int32_t main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    
+    int n; 
+}
--- Du Tuyen/a.cpp
+++ Du Tuyen/a.cpp
@@ -1,107 +1,104 @@
-//Author : Trieu self-destruct
-//1st DHBB
-//1st O30/4 (11 :<)
-//TST next year
+// problem "a"
+// created in 09:59:05 - Sat 20/07/2024
 
 #include <bits/stdc++.h>
-//#include <ext/pb_ds/assoc_container.hpp>
-//#include <ext/pb_ds/tree_policy.hpp>
 
-//using namespace __gnu_pbds;
+// #define int int64_t
 
 using namespace std;
 
-/*Pragma*/
-#pragma GCC optimize("Ofast")
-//#pragma GCC optimize("O3")
-#pragma GCC target("avx,avx2,fma")
-#pragma GCC optimization ("unroll-loops")
-//#pragma GCC optimize("Os")
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+ 
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ", "; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define dbg(x...) cerr << "\e[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _print(x); cerr << "\e[39m" << endl;
+#else
+#define dbg(x...)
+#endif
 
-/*Weird define*/
-#define ll long long
-//#define endl '\n'
-#define gcd __gcd
-//#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
-//#define p prev
-#define count1 __builtin_popcount
-
-/*For define*/
-#define For(i,a,b) for(ll i=a;i<=b;i++)
-#define foR(i,a,b) for(ll i=a;i>=b;i--)
-#define FOR(i,a,b) for(ll i=a;i<b;i++)
-#define forN(i,a,b,c) for(ll i=a;i<=b;i+=c)
-#define foRN(i,a,b,c) for(ll i=a;i>=b;i-=c)
-#define fora(i, a) for (auto i : a)
-
-/*push-pop-pair define*/
-#define pb push_back
-#define pob pop_back
-#define pf push_front
-#define pof pop_front
-#define x first
-#define y second
-
-/*Print define*/
-#define write(a, l, r) for (ll ik = l; ik <= r; ik++) cout << a[ik] << ' '; cout << endl
-#define writeln(a, l, r) for (ll ik = l; ik <= r; ik++) cout << a[ik] << endl
-
-/*Type-def*/
-typedef unsigned long long 	ull;
-typedef vector<ll> 		vi;
-typedef pair<ll, ll>		ii;
-typedef long double         ld;
-
-/*some const*/
-const ll IMA = LLONG_MAX-10;
-const ll IMI = LLONG_MIN+10;
-const ll INF = INT_MAX;
-const ll MOD = 1e9+7;
-
-/* Calculation with MODULO*/
-ll dec(ll x, ll y) {return (x-y+MOD)%MOD;}
-ll add(ll x, ll y) {return (x + y) % MOD;}
-ll mul(ll x, ll y) {return ((x%MOD) * (y%MOD)) % MOD;}
-ll bpow(ll x, ll y) {ll res = 1; while (y) {if (y & 1) res = mul(res, x); x = mul(x, x); y >>= 1;} return res;}
-ll ModInverse(ll x) {return bpow(x, MOD - 2);}
-ll Div(ll x, ll y) {return mul(x, ModInverse(y));}
-ll GetBit(ll val, ll num) {return ((val>>num) & 1LL);}
-
-/*Some simple checking and calculation function*/
-string yn(bool k){if (k) return "YES"; else return "NO";}
-ll binpower(ll base, ll e, ll mod) {ll result = 1;base %= mod;while (e){if (e & 1)result = (ll)result * base % mod;base = (ll)base * base % mod;e >>= 1;    }return result;}
-bool check_composite(long long n, long long a, long long d, int s) {long long x = binpower(a, d, n); if (x == 1 or x == n - 1)return false;for (int r = 1; r < s; r++) {x = (long long)x * x % n;if (x == n - 1)return false;}return true;};
-bool checkprime(long long n) {if (n < 2)return false;int r = 0;long long d = n - 1;while ((d & 1) == 0) {d >>= 1;r++;}for (int a : {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}) {if (n == a)return true;if (check_composite(n, a, d, r))return false;}return true;}
-bool checksqr(ll k){return (trunc(sqrt(k))*trunc(sqrt(k))==k);}
-ll lcm(ll a, ll b){return (a*b)/gcd(a,b);}
-
-const int N=2e5+100;
-ll f[N];
-
-signed main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    //freopen("GRAPHDIV.INP","r",stdin);
-    //freopen("GRAPHDIV.OUT","w",stdout);
-    ll n,m;
-    cin >> n >> m;
-    f[0]=f[1]=1;
-    For(i,2,N) f[i]=(f[i-1]+f[i-2])%MOD;
-    cout << mul(2,dec(add(f[n],f[m]),1));
+vector<int> prefix_function(string s) {
+    int n = (int) s.length();
+    vector<int> pi(n);
+    for(int i = 1; i < n; i++) {
+        int j = pi[i - 1];
+        while(j > 0 && s[i] != s[j])
+            j = pi[j - 1];
+        if(s[i] == s[j])
+            j++;
+        pi[i] = j;
+    }
+    return pi;
 }
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string a, b;
+    cin >> a >> b;
+    int n = a.size(), m = b.size();
+    if(n > m)
+        swap(m, n), swap(a, b);
+    dbg(n) 
+    dbg(m)
+    vector<int> u;
+    for(int i = 1; i <= sqrt(n); ++i) {
+        if(n % i == 0 && m % i == 0) {
+            u.push_back(i);
+            if(i * i != n && m % (n / i) == 0)
+                u.push_back(n / i);
+        }
+    }
+    dbg(u)
+    string c = a + '#' + b;
+    vector<int> pi = prefix_function(c), ans(n + m + 1, 1);
+    vector<int> p = prefix_function(a), s(n + 1, 1);
+    dbg(pi)
+    dbg(p)
+    cerr << "c: " << c << '\n';
+    // exit(0);
+    int res = 0;
+
+    for(int i = n + m; i > 0; i--)
+        if(pi[i])
+            ans[pi[i] - 1] += ans[i];
+
+    for(int i = n - 1; i > 0; i--)
+        if(p[i])
+            s[p[i] - 1] += s[i];
+    dbg(ans)
+    dbg(s)
+    for(auto v: u) {
+        for(int i = n + v; i <= m + n; i += v) {
+            if(pi[i] >= m / v && p[n - v - 1] >= n / v)
+                res++;
+        }
+    }
+
+    cout << res;
+}
+
 /*
-//Delete define endl when interactive prolem
-
-INPUT :
-4 1
-1 1 1 2
-
-OUTPUT :
-
-If it WA, check :
-- Special case (Usually, n=1)
-- WRONG FORMAT OUTPUT
-- Check reading
-- Change (ll) to (ull)
-- lleger Overflow (The number that bigger than 2^63-1)
+abcd
+abcdabcd
+abcd#abcdabcd
+0123456789012
 */
