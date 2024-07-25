a6232816bfaf865eabdcdc34a0604e07c738d48d
ntannn <nhattanff07@gmail.com>
2024-07-25 14:00:01 +0700

    ok

--- Du Tuyen/a.cpp
+++ Du Tuyen/a.cpp
@@ -1,33 +1,19 @@
-// problem "a"
-// created in 15:54:23 - Wed 24/07/2024
-
-#include <bits/stdc++.h>
-
-#define int int64_t
-#define x first
-#define y second
+#include<bits/stdc++.h>
 
 using namespace std;
 
-int32_t main() {
+typedef long long ll;
+
+
+signed main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int t;
-    cin >> t;
-    while(t--) {
-        pair<int, int> a, b, c;
-
-        cin >> a.x >> a.y >> b.x >> b.y >> c.x >> c.y;
-
-        int ans = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
-
-        if(ans > 0)
-            cout << "LEFT";
-        else if(ans < 0)
-            cout << "RIGHT";
-        else
-            cout << "TORCH";
-        cout << '\n';
+    int test;
+    cin >> test;
+    while(test--) {
+        ll l, r;
+        cin >> l >> r;
+        
     }
 }
