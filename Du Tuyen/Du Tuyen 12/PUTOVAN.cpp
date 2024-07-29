61597595fe560e9c79e053571956f1399c5bea71
ntanthedev <nhattanff07@gmail.com>
2024-07-29 10:02:53 +0700

    ok

--- Du Tuyen/Du Tuyen 12/PUTOVAN.cpp
+++ Du Tuyen/Du Tuyen 12/PUTOVAN.cpp
@@ -1,5 +1,5 @@
 //Written by: ntannn_
-//created in 08:38:25 - Mon 29/07/2024
+//created in 09:27:13 - Mon 29/07/2024
 #include <bits/stdc++.h>
 // #pragma GCC optimize("O3,unroll-loops,no-stack-protector")
 // #pragma GCC target("sse4,avx2,fma")
@@ -19,16 +19,53 @@ typedef pair<ll, ll> pll;
 typedef vector<int> vi;
 typedef vector<ll> vll;
 
-const int N = 1e6 + 10;
+const int N = 5e4 + 10;
 const ll MOD = 1e9 + 7;
 
+int n, m;
+vi adj[N], ans;
+int d[N];
+bool p[N];
 
-
-void solve() {
-    
+void dfs(int u, int h) {
+    if(h == 0) 
+        return;
+    p[u] = 0;
+    for(int v : adj[u]) {
+        dfs(v, max(h - 1, d[v]));
+    }
 }
 
-#define task "a"
+void solve() {
+    cin >> n >> m;
+
+    for(int i = 1, u, v; i <= m; ++i) {
+        cin >> u >> v;
+        adj[u].eb(v);
+        adj[v].eb(u);
+    }
+
+    for(int i = 1; i <= n; ++i) {
+        cin >> d[i];
+        p[i] = (d[i] < 1);
+    }
+
+    for(int i = 1; i <= n; ++i) {
+        if(d[i] > 0) 
+            dfs(i, d[i]); 
+    }
+
+    for(int i = 1; i <= n; ++i) 
+        if(p[i])
+            ans.eb(i);
+    
+    cout << ans.size() << '\n';
+
+    for(auto i : ans)
+        cout << i << " " ;
+}
+
+#define task "PUTOVAN"
 signed main() {
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
