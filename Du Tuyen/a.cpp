b5165c730754af7f5dee9e34c7314b240af03ab3
ntannn <nhattanff07@gmail.com>
2024-07-30 14:17:32 +0700

    ok

--- Du Tuyen/a.cpp
+++ Du Tuyen/a.cpp
@@ -1,45 +1,33 @@
-//Written by: ntannn_
-//created in 08:38:25 - Mon 29/07/2024
-#include <bits/stdc++.h>
-// #pragma GCC optimize("O3,unroll-loops,no-stack-protector")
-// #pragma GCC target("sse4,avx2,fma")
-#define fi first
-#define se second
-#define mp make_pair
-#define pb push_back
-#define eb emplace_back
-#define all(x) x.begin(), x.end()
-#define TIME (1.0 * clock() / CLOCKS_PER_SEC)
+//problem "a"
+//created in 01:57:55 - Tue 30/07/2024
+
+#include<bits/stdc++.h>
+
+#define int int64_t
 
 using namespace std;
 
-typedef long long ll;
-typedef pair<int, int> ii;
-typedef pair<ll, ll> pll;
-typedef vector<int> vi;
-typedef vector<ll> vll;
+int n;
 
-const int N = 1e6 + 10;
-const ll MOD = 1e9 + 7;
-
-
-
-void solve() {
-    
+void cal(int i, string ans) {
+    if(i > n) {
+        cout << ans << '\n';
+        return;
+    }
+    for(char j = '0'; j <= '1'; j++) {
+        if(i == 1 || (ans.back() != '1' || j == '0')) {
+            ans.push_back(j);
+            cal(i + 1, ans);
+            ans.pop_back();
+        }
+    }
 }
 
-#define task "a"
-signed main() {
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
-    if(fopen(task ".inp", "r")) {
-        freopen(task ".inp", "r", stdin);
-        freopen(task ".out", "w", stdout);
-    }
-    int test_case = 1;
-    //cin >> test_case;
-    while(test_case--) {
-        solve();
-    }
-    // cerr << '\n' << "\x1b[31mtime is: " << TIME << "\e[39m";
+int32_t main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    cin >> n;
+
+    string ans = "";
+    cal(1, ans);
 }
