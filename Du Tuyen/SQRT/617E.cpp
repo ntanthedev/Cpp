65b8134593896c26cff8ee73ea1ccc3ed6a30d44
ntannn <nhattanff07@gmail.com>
2024-08-06 23:17:47 +0700

    ok

--- Du Tuyen/SQRT/617E.cpp
+++ Du Tuyen/SQRT/617E.cpp
@@ -3,12 +3,13 @@
 
 #include<bits/stdc++.h>
 
+#define int long long
 const int N = 5e5 + 5;
 
 using namespace std;
 
 int n, k, m, sz, ans = 0;
-int cnt[1 << 20], a[N], res[N];
+int cnt[4 * N], a[N], res[N], s[N];
 
 struct query {
     int l, r, id;
@@ -19,14 +20,14 @@ struct query {
 
 } q[N];
 
-void add(int i) {
-    ans += cnt[a[i] ^ k];
-    cnt[a[i]]++;
+void add(int x) {
+    ans += cnt[x ^ k];
+    cnt[x]++;
 }
 
-void rmv(int i) {
-    cnt[a[i]]--;
-    ans -= cnt[a[i] ^ k];
+void rmv(int x) {
+    cnt[x]--;
+    ans -= cnt[x ^ k];
 }
 
 int32_t main() {
@@ -35,9 +36,11 @@ int32_t main() {
     cin >> n >> m >> k;
     
     sz = sqrt(n);
+    s[0] = 0;
     
     for(int i = 1; i <= n; i++) {
         cin >> a[i];
+        s[i] = s[i - 1] ^ a[i];
     }
 
     for(int i = 1; i <= m; i++) {
@@ -48,12 +51,12 @@ int32_t main() {
     sort(q + 1, q + 1 + m);
 
     int l = 1, r = 0;
-
+    add(s[0]);
     for(int i = 1; i <= m; i++) {
-        while(r < q[i].r) add(++r);
-        while(r > q[i].r) rmv(r--);
-        while(l > q[i].l) add(--l);
-        while(l < q[i].l) rmv(l--);
+        while(r < q[i].r) add(s[++r]);
+        while(r > q[i].r) rmv(s[r--]);
+        while(l > q[i].l) add(s[--l - 1]);
+        while(l < q[i].l) rmv(s[l++ - 1]);
         res[q[i].id] = ans;
     }
 
