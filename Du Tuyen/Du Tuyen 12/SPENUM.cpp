c8809f88b5c4edb373d2f3a9d5616dd3e132dd4a
ntanthedev <nhattanff07@gmail.com>
2024-08-10 09:06:57 +0700

    ok

--- Du Tuyen/Du Tuyen 12/SPENUM.cpp
+++ Du Tuyen/Du Tuyen 12/SPENUM.cpp
@@ -5,47 +5,40 @@
 
 // #define int int64_t
 #define TIME (1.0 * clock() / CLOCKS_PER_SEC)
-const int N = 1e6 + 9;
+const int N = 1e6;
 
 using namespace std;
 
-int count_division (int x) {
-    int t = sqrt(x), ans = 0;
-    for(int i = 1; i <= t; i++) {
-        if(x % i == 0) {
-            ans++;
-            if(x/i != i)
-                ans++;
-        }
-    }
-    return ans;
-}
-
-vector<int> p(N, 0), s(N, 0);
+vector<int> p(N + 10, 1), s(N + 10, 0);
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL);
     
-    // freopen()
-    freopen("a.txt","w",stdout);
-
-    // memset(p, 1, sizeof(p));
+    #define task "SPENUM"
+    if(fopen(task ".inp", "r")) {
+        freopen(task ".inp", "r", stdin);
+        freopen(task ".out", "w", stdout);
+    }
 
     for(int i = 2; i <= (int)1e6; i++) {
         for(int j = i; j <= (int)1e6; j += i) {
             p[j]++;
         }
     }
+
+    s[0] = 0;
+    
     for(int i = 1; i <= (int)1e6 + 5; i++) {
         s[i] = s[i - 1] + (p[i] == 9);
     } 
 
-    cout << "{" ;
-    for(int i = 0; i < s.size(); i++) {
-        if(i > 0) 
-            cout << ",";
-        cout << s[i];
-    } 
+    int t;
+    cin >> t;
+    while(t--) {
+        int a, b;
+        cin >> a >> b;
+        cout << s[b] - s[a - 1] << '\n';
+    }
     
     cerr << '\n' << "\x1b[31mtime is: " << TIME << "\e[39m";
 }
