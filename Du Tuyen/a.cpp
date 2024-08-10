b3d205e91cae98a8aa01f2e7f0f7fc2c754c403c
ntannn <nhattanff07@gmail.com>
2024-08-10 15:03:59 +0700

    ok

---
+++ Du Tuyen/a.cpp
@@ -0,0 +1,14 @@
+//problem "a"
+//created in 15:03:18 - Sat 10/08/2024
+
+#include<bits/stdc++.h>
+
+#define int int64_t
+
+using namespace std;
+
+int32_t main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    
+    
+}
