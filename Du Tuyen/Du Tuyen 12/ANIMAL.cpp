be9006e1051f5642ff6b3959b7ec83f00339945d
ntanthedev <nhattanff07@gmail.com>
2024-08-13 10:30:55 +0700

    sadf

---
+++ Du Tuyen/Du Tuyen 12/ANIMAL.cpp
@@ -0,0 +1,12 @@
+//problem "ANIMAL"
+//created in 09:38:14 - Tue 13/08/2024
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int32_t main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    
+    
+}
--- Du Tuyen/a.cpp
+++ Du Tuyen/a.cpp
@@ -1,15 +1,45 @@
-//problem "a"
-//created in 23:00:05 - Mon 12/08/2024
-
-#include<bits/stdc++.h>
-
-#define int int64_t
+//Written by: ntannn_
+//created in 09:28:35 - Tue 13/08/2024
+#include <bits/stdc++.h>
+// #pragma GCC optimize("O3,unroll-loops,no-stack-protector")
+// #pragma GCC target("sse4,avx2,fma")
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define all(x) x.begin(), x.end()
+#define TIME (1.0 * clock() / CLOCKS_PER_SEC)
 
 using namespace std;
 
-int32_t main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+const int N = 1e6 + 10;
+const ll MOD = 1e9 + 7;
+
+
+
+void solve() {
     
-    int n;
-    cin >> n;
+}
+
+#define task "a"
+signed main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    if(fopen(task ".inp", "r")) {
+        freopen(task ".inp", "r", stdin);
+        freopen(task ".out", "w", stdout);
+    }
+    int test_case = 1;
+    //cin >> test_case;
+    while(test_case--) {
+        solve();
+    }
+    // cerr << '\n' << "\x1b[31mtime is: " << TIME << "\e[39m";
 }
