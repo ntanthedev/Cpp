daac47e6738141d2492b4108dd523cecdd58f225
ntannn <nhattanff07@gmail.com>
2024-08-24 00:40:28 +0700

    ok

--- Du Tuyen/b.cpp
+++ Du Tuyen/b.cpp
@@ -1,52 +1,39 @@
-#include<cstdio>
-#include<algorithm>
-#include<cstring>
-#include<vector>
-#include<set>
-using namespace std;
-int C[101000], CC[101000], cnt, res;
-int n, w[101000];
-set<int>Set;
-int main() {
+//template simple for codeforces by ntannn_
+#include <bits/stdc++.h>
+#define int int64_t
 
-    if(fopen("test.inp","r")) {
-        freopen("test.inp","r",stdin);
-        freopen("b.out","w",stdout);
-    }
-	int i;
-	scanf("%d", &n);
-	for (i = 1; i <= n; i++) {
-		scanf("%d", &w[i]);
-		if (!C[w[i]]) {
-			cnt++;
-		}
-		else {
-			CC[C[w[i]]]--;
-			if (!CC[C[w[i]]])Set.erase(C[w[i]]);
-		}
-		C[w[i]]++;
-		CC[C[w[i]]]++;
-		Set.insert(C[w[i]]);
-		if (cnt == 1) {
-			res = i;
+using namespace std;
+
+void solve() {
+	int n, m, ans;
+	cin >> n >> m;
+	vector<int> par(n + 1);
+	for(int i = 1; i <= n; i++) 
+		par[i] = i;
+
+	function<int(int)> find_par = [&](int x) {
+		return x == par[x] ? x : par[x] = find_par(par[x]);
+	};
+
+	ans = 0;
+	for(int i = 1; i <= m; i++) {
+		int u, v;
+		cin >> u >> v;
+		if(u == v)
 			continue;
-		}
-		if (CC[1] == cnt) {
-			res = i;
-			continue;
-		}
-		int t = C[w[i]];
-		if (Set.size() == 2) {
-			if (CC[1] == 1) {
-				res = i;
-			}
-			else {
-				auto it = Set.end();
-				it--;
-				int x = *it;
-				if (CC[x] == 1 && CC[x - 1] == cnt - 1)res = i;
-			}
-		}
+		if(find_par(v) == find_par(u))
+			ans++;
+		else
+			par[u] = v;
+		ans++;
 	}
-	printf("%d\n", res);
+	cout << ans << '\n';
+}
+
+int32_t main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int test_case;
+	for(cin >> test_case;test_case--;cout << '\n') 
+		solve();
 }
