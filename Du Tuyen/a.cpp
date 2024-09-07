cc5b96bbd03311d16c6e6c0088e36e0a8d275386
ntanthedev <nhattanff07@gmail.com>
2024-09-07 09:17:28 +0700

    ok

--- Du Tuyen/a.cpp
+++ Du Tuyen/a.cpp
@@ -1,47 +1,14 @@
-<<<<<<< HEAD
-=======
-//Written by: ntannn_
-//created in 08:14:39 - Fri 23/08/2024
-#include <bits/stdc++.h>
+//problem "a"
+//created in 09:05:43 - Sat 07/09/2024
 
-#define fi first
-#define se second
-#define mp make_pair
-#define pb push_back
-#define eb emplace_back
-#define all(x) x.begin(), x.end()
-#define TIME (1.0 * clock() / CLOCKS_PER_SEC)
+#include<bits/stdc++.h>
+
+#define int int64_t
 
 using namespace std;
 
-typedef long long ll;
-typedef pair<int, int> ii;
-typedef pair<ll, ll> pll;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-
-const int N = 1e6 + 10;
-const ll MOD = 1e9 + 7;
-
-
-
-void solve() {
+int32_t main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    
     
 }
-
-#define task "a"
-signed main() {
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
-    if(fopen(task ".inp", "r")) {
-        freopen(task ".inp", "r", stdin);
-        freopen(task ".out", "w", stdout);
-    }
-    int test_case = 1;
-    //cin >> test_case;
-    while(test_case--) {
-        solve();
-    }
-    // cerr << '\n' << "\x1b[31mtime is: " << TIME << "\e[39m";
-}
->>>>>>> 5ea6772cf3d9db8cfbded0c6d3ac4e68d262083b
