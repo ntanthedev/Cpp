c017dc48ec80b9930625a5bc079854e491716fa8
ntannn <nhattanff07@gmail.com>
2024-09-08 15:42:41 +0700

    ok

---
+++ Du Tuyen/a.cpp
@@ -0,0 +1,26 @@
+//problem "a"
+//created in 15:40:41 - Sun 08/09/2024
+
+#include<bits/stdc++.h>
+
+#define int int64_t
+
+using namespace std;
+
+int32_t main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    
+    int n, m, x, y, k;
+
+    cin >> n >> m >> x >> y >> k;
+
+    vector<int> a(n), b(m);
+
+    for(int &i : a) cin >> i;
+
+    for(int &i : b) cin >> i;
+
+    bool use_x = (x < k);
+
+    
+}
