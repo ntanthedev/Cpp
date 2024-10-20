1a2d7574e86c35fa552ff8475c8b953073ac21a9
ntannn <nhattanff07@gmail.com>
2024-10-20 14:41:36 +0700

    ọc ọc icpc

--- Du Tuyen/a.cpp
+++ Du Tuyen/a.cpp
@@ -1,22 +1,36 @@
+//problem "a"
+//created in 09:00:38 - Sun 20/10/2024
+
 #include<bits/stdc++.h>
+
+#define int int64_t
+
 using namespace std;
-int main()
-{
-    long long a;
-    cin>>a;
-    if(a>=1)
-    {
-        cout<<"1";
+
+int32_t main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    
+    int n, q;
+
+    cin >> n >> q;
+
+    vector<set<int>> adj(n + 1);
+
+    for(int i = 1, u, v; i < n; i++) {
+        cin >> u >> v;
+        adj[u].insert(v);
+        adj[v].insert(u);
     }
-    else 
-    {
-        if(a <= -1)
-        {
-            cout << -1;
-        }
+
+    while(q--) {
+        int x, z, y;
+
+        cin >> x >> y >> z;
+
+        if(adj[x].count(y) && adj[y].count(z))
+            cout << "YES";
         else 
-        {
-            cout << 0;
-        }
+            cout << "NO";
+        cout << '\n';
     }
 }
