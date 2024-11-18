7f606791499026c6e692ad687ba127c1bf8be191
ntannn <nhattanff07@gmail.com>
2024-11-18 16:57:15 +0700

    Refactor checker logic and remove obsolete answer file

--- VNOI/checker/checker.cpp
+++ VNOI/checker/checker.cpp
@@ -1,81 +1,87 @@
 #include "testlib.h"
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-const int N = 1e3 + 5;
-typedef long long ll;
-
+const int MAXN = 1009;
 int n, m;
-bool w[N][N];
+bool adj[MAXN][MAXN];
 
 int main(int argc, char* argv[]) {
     registerTestlibCmd(argc, argv);
-
-    int ouf_ans = ouf.readInt();
-    int ans_ans = ans.readInt();
     
-    m = inf.readInt();
+    // Read input
     n = inf.readInt();
-    int a = inf.readInt(), b = inf.readInt();
-
-    for(int i = 1; i <= m; i++) 
-        for(int j = 1; j <= n; j++) 
-            w[i][j] = inf.readInt();
-
-    if(ans_ans == 0) {
-        if(ouf_ans != 0) {
-            quitp(0.0, "Me cung khong co duong di nhung nha tham hien co duong di");
-        }
-        else {
-            quitp(100.0, "Ket qua dung");
-        }
-    } 
-    else {
-        if(ouf_ans == 0) {
-            quitp(0.0, "Ket qua sai");
-        }
-        else {
-            bool ok = false;
-            vector<pair<int, int>> res;
-            for(int i = 1; i <= ouf_ans; i++) {
-                int x = ouf.readInt(), y = ouf.readInt();
-                if(w[x][y]) {
-                    quitp(0.0, "Di chuyen den o (%d, %d) co cam bay", x, y);
-                }
-                res.push_back({x, y});
-            }
-
-            if(!ouf.seekEof()) {
-                quitp(0.0, "File output con thua");
-            }
-
-            if(make_pair(a, b) != res[0]) {
-                quitp(0.0, "Diem bat dau khong dung");
-            }
-
-            if(res.back().first != 1 && res.back().first != m && res.back().second != 1 && res.back().second != n) {
-                quitp(0.0, "Nha tham hien chua thoat khoi me cung");
-            }
-
-            auto [x, y] = res.front();
-
-            for(int i = 1; i < res.size(); i++) {
-                if(abs(res[i].first - x) + abs(res[i].second - y) != 1) {
-                    quitp(0.0, "Tu diem %lld, %lld khong the den diem %lld, %lld", x, y, res[i].first, res[i].second);
-                }
-                x = res[i].first;
-                y = res[i].second;
-            }
-
-            if(ouf_ans < ans_ans) 
-                quitp(150.0, "Output toi uu hon ket qua");
-            else if(ouf_ans == ans_ans)
-                quitp(100.0, "Ket qua dung");
-            else 
-                quitp(50.0, "Ket qua chua toi uu");
-        }
+    m = inf.readInt();
+    for(int i = 0; i < m; i++) {
+        int u = inf.readInt();
+        int v = inf.readInt();
+        adj[u][v] = true;
     }
 
+    // Read answers
+    int jury_ans = ans.readInt();
+    int contestant_ans = ouf.readInt();
+
+    if (contestant_ans != jury_ans) {
+        quitf(_wa, "Wrong answer\nExpected: %d\nFound: %d", jury_ans, contestant_ans);
+    }
+
+    if (contestant_ans == -1) {
+        quitf(_ok, "Correct answer: -1");
+    }
+
+    // Validate first robot path
+    int start1 = ouf.readInt();
+    if (start1 != 1) {
+        quitf(_wa, "Robot 1 must start from vertex 1, found: %d", start1);
+    }
+
+    int path_length1 = 0;
+    int prev = start1;
+    while (!ouf.seekEof()) {
+        int next = ouf.readInt();
+        if (next == 0) {
+            quitf(_wa, "Invalid vertex: 0");
+        }
+        if (!adj[prev][next]) {
+            quitf(_wa, "No edge exists: %d->%d", prev, next);
+        }
+        prev = next;
+        path_length1++;
+    }
+
+    if (path_length1 != contestant_ans) {
+        quitf(_wa, "Robot 1 path length mismatch. Expected: %d, Found: %d", 
+              contestant_ans, path_length1);
+    }
+
+    // Validate second robot path
+    int start2 = ouf.readInt();
+    if (start2 != n) {
+        quitf(_wa, "Robot 2 must start from vertex %d, found: %d", n, start2);
+    }
+
+    int path_length2 = 0;
+    prev = start2;
+    while (!ouf.seekEof()) {
+        int next = ouf.readInt();
+        if (next == 0) {
+            quitf(_wa, "Invalid vertex: 0");
+        }
+        if (!adj[prev][next]) {
+            quitf(_wa, "No edge exists: %d->%d", prev, next);
+        }
+        prev = next;
+        path_length2++;
+    }
+
+    if (path_length2 != contestant_ans) {
+        quitf(_wa, "Robot 2 path length mismatch. Expected: %d, Found: %d", 
+              contestant_ans, path_length2);
+    }
+
+    // If we got here, everything is correct
+    quitf(_ok, "Correct");
+    
     return 0;
 }
