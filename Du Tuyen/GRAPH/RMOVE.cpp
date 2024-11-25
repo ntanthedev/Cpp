15f51a445b8b22e97034db656edbd5633d63da43
ntannn <nhattanff07@gmail.com>
2024-11-25 17:16:56 +0700

    Add debugging macros and improve code readability; comment out unused variables and structures

--- Du Tuyen/GRAPH/RMOVE.cpp
+++ Du Tuyen/GRAPH/RMOVE.cpp
@@ -3,9 +3,36 @@
 
 #include<bits/stdc++.h>
 
-#define int int64_t
+// #define int int64_t
 
 using namespace std;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+ 
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ", "; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define dbg(x...) cerr << "\e[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _print(x); cerr << "\e[39m" << endl;
+#else
+#define dbg(x...)
+#endif
 
 int32_t main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL);
@@ -21,7 +48,7 @@ int32_t main() {
     cin >> n >> m;
 
     vector<vector<int>> adj(n + 1, vector<int>());
-    vector<vector<int>> h(n + 1, vector<int>(2, 0));
+    // vector<vector<int>> h(n + 10, vector<int>(2, 0));
     vector<int> res, path;
     vector<bool> b(n + 1, 0);
 
@@ -35,7 +62,7 @@ int32_t main() {
         return cout << 0 << '\n' << 1 << '\n' << 1, signed();
 
 
-    h[1] = {1, 1};
+    // h[1] = {1, 1};
     
     function<vector<vector<int>>(int, int)> bfs = [&](int be, int en) {
         vector<vector<int>> d(n + 1, vector<int>(2, INT_MAX));
@@ -70,18 +97,21 @@ int32_t main() {
     vector<vector<int>> nguoc = bfs(n, 1);
     vector<vector<int>> xuoi = bfs(1, n);
 
+    dbg(nguoc);
+    exit(0);
+
     int minn = INT_MAX;
     bool flag = 0;
 
     for(int i = 1; i <= n; i++) {
         if(nguoc[i][0] != INT_MAX && xuoi[i][0] != INT_MAX) {
-            if(nguoc[i][0] + xuoi[i][0]  - 1 < minn) {
+            if(nguoc[i][0] + xuoi[i][0] - 1 < minn) {
                 minn = nguoc[i][0] + xuoi[i][0] - 1;
             }
             flag = 1;
         }
         if(nguoc[i][1] != INT_MAX && xuoi[i][1] != INT_MAX) {
-            if(nguoc[i][1] + xuoi[i][1]  - 1 < minn) {
+            if(nguoc[i][1] + xuoi[i][1] - 1 < minn) {
                 minn = nguoc[i][1] + xuoi[i][1] - 1;
             }
             flag = 1;
