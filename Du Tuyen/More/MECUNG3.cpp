847d2242c2d6fd62566ff69d71bc27c9b846f573
ntannn <nhattanff07@gmail.com>
2024-11-30 19:27:57 +0700

    a

--- Du Tuyen/More/MECUNG3.cpp
+++ Du Tuyen/More/MECUNG3.cpp
@@ -1,104 +1,53 @@
 #include <bits/stdc++.h>
-#define fi first
-#define se second
-#define mp make_pair
-#define pb push_back
-#define eb emplace_back
-#define lb lower_bound
-#define ub upper_bound
-#define all(x) x.begin(), x.end()
+#pragma GCC optimize("O3,unroll-loops,no-stack-protector")
+#pragma GCC target("sse4,avx2,fma")
+#define int long long
 using namespace std;
+int new_x, new_y, hx[4] = {0, 0, -1, 1}, hy[4] = {1, -1, 0, 0}, x, y, n, m, q1,
+                  q2, f[1002][1002], a[1002][1002];
+long long t;
+bool check[1002][1002];
+queue<pair<int, int>> q;
+void bfs() {
+    q.push({x, y});
+    f[x][y] = 0;
+    while(!q.empty()) {
+        pair<int, int> u = q.front();
+        q.pop();
 
-const int N = 1e3+10;
+        for(int i = 0; i < 4; i++) {
+            new_x = u.first + hx[i];
+            new_y = u.second + hy[i];
 
-typedef long double ld;
-typedef long long ll;
-typedef pair<int,int> ii;
-typedef vector<int> vi;
-typedef vector<vi> vvi;
-typedef vector<ii> vii;
-
-ll m, n, Q;
-int dx[4] = {1, -1, 0, 0};
-int dy[4] = {0, 0, 1, -1};
-ii b;
-queue<ii> q;
-int a[N][N];
-ll d[N][N];
-bool visited[N][N];
-vii ans;
-bool flag = 1;
-
-bool check( int x2, int y2){ 
-    if(x2 < 1 || y2 < 1 || x2 > m || y2 > n)    
-        return false;
-    return true;
-}
-
-void solve(){
-    q.push(b);
-    visited[b.fi][b.se] = true;
-    while(!q.empty()){
-        ii u = q.front(); q.pop();
-        for(int i = 0; i < 4; i++){
-            ll nx = u.fi + dx[i], ny = u.se + dy[i];
-            if(check( nx, ny)){
-                if(a[u.fi][u.se] != a[nx][ny] && d[nx][ny] + 1 < d[u.fi][u.se]){
-                    flag = 0;
-                    d[u.fi][u.se] = d[nx][ny] + 1;
-                }
-                if(a[u.fi][u.se] == a[nx][ny] && d[nx][ny] < d[u.fi][u.se]){
-                    flag = 0;
-                    d[u.fi][u.se] = d[nx][ny];
-                }
-                if(!visited[nx][ny]){
-                    visited[nx][ny] = true;
-                    q.push(mp(nx, ny));
-                }
+            if(new_x < 1 || new_x > n || new_y < 1 || new_y > m)
+                continue;
+            if(a[new_x][new_y] != a[u.first][u.second] && f[new_x][new_y] > f[u.first][u.second] + 1) {
+                q.push({new_x, new_y});
+                f[new_x][new_y] = f[u.first][u.second] + 1;
+            }
+            if(a[new_x][new_y] == a[u.first][u.second] && f[new_x][new_y] > f[u.first][u.second]) {
+                q.push({new_x, new_y});
+                f[new_x][new_y] = f[u.first][u.second];
             }
         }
     }
-
-    ii e;
-    while(Q--){
-        cin >> e.fi >> e.se;
-        if(d[e.fi][e.se] != 1e18)
-            cout << d[e.fi][e.se] << '\n';
-        else
-            cout << -1 << '\n';
-    }
-
-    // cout << "-----------\n";
-    // for(int i = 1; i <= m; i++){
-    //     for(int j = 1; j <= n; j++)
-    //         if(d[i][j] == 1e18)
-    //             cout << 9 << " ";
-    //         else
-    //             cout << d[i][j] << " ";
-    //     cout << '\n';
-    // }
 }
-
-void init(){
-    cin >> m >> n >> Q;
-    cin >> b.fi >> b.se; 
-    for(int i = 1; i <= m; i++)
-        for(int j = 1; j <= n; j++){
-            cin >> a[i][j];
-            visited[i][j] = false;
-            d[i][j] = 1e18;
-        }
-    d[b.fi][b.se] = 0;
-}
-#define task "MECUNG3"
-int main(){
-    cin.tie(NULL);
+int32_t main() {
     ios_base::sync_with_stdio(false);
-    if(fopen(task".inp","r")){
-        freopen(task".inp","r",stdin);
-        freopen(task".out","w",stdout);
+    cin.tie(NULL);
+    freopen("mecung3.inp", "r", stdin);
+    freopen("mecung3.out", "w", stdout);
+    cin >> n >> m >> t;
+    cin >> x >> y;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= m; j++) {
+            cin >> a[i][j];
+            f[i][j] = LLONG_MAX - 5;
+        }
     }
-    init();
-    solve();
-    return 0;
+    bfs();
+    while(t--) {
+        cin >> q1 >> q2;
+        cout << f[q1][q2] << "\n";
+    }
 }
