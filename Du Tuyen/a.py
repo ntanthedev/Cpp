1768063e2833a05a4ad7044ec0b1ab40b2a609eb
ntanthedev <nhattanff07@gmail.com>
2025-01-04 09:12:06 +0700

    ok

--- Du Tuyen/a.py
+++ Du Tuyen/a.py
@@ -1,1 +1,13 @@
-a = int(input())
+import math
+with open("bai1.INP","r") as r:
+    n, k = int(r.readline().split())
+
+ans = -1
+if n > 1:
+    for i in range(2, math.sqrt(n) + 1):
+        if n % i == 0:
+            ans = n // i
+            break
+
+with open("bai1.OUT","w") as w:
+    w.write(int(ans))
