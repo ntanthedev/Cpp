cf2b812aaeece68b1d947076e5e4b70b2ed81f58
ntan <nhattanff07@gmail.com>
2025-05-28 15:44:28 +0700

    a

--- Du Tuyen/a.cpp
+++ Du Tuyen/a.cpp
@@ -5,8 +5,27 @@ using namespace std;
 typedef long long ll;
 const int N = 1e6 + 10;
 
-ll n, a[N], m = 0;
+ll n, a[N], m = 0, s[N];
 map<ll, ll> tmp;
+
+vector<int> merge(vector<int> x, vector<int> y) {
+    int i = 0, j = 0;
+    vector<int> z;
+
+    while(i < x.size() && j < y.size()) {
+        if(x[i] < y[j]) 
+            z.push_back(x[i++]);
+        else 
+            z.push_back(y[j++]);
+    }
+
+    while(i < x.size()) 
+        z.push_back(x[i++]);
+    while(j < y.size())
+        z.push_back(y[j++]);
+
+    return z;
+}
 
 int32_t main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL);
@@ -24,5 +43,7 @@ int32_t main() {
     for(int i = 1; i <= n; i++) 
         a[i] = tmp[a[i]];
 
-    
+    for(int i = 1; i <= n; i++) {
+        s[a[i]]++;
+    }
 }
